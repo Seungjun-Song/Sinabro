ee030d7da9e0aa81359068f71a2a6fa73f61d6f2
JongKook <whdrnrdl789@naver.com>
2024-04-19 01:56:07 +0900

    Feat: Simple OAuth Test

---
+++ be/src/main/java/com/HP50/be/config/SecurityConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1b21d55aa87f64cf46e87dd721e7be36d69df049e9b09d2423fa05162ca92c6
+size 849
---
+++ be/src/main/java/com/HP50/be/controller/HpController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d89f18db58ec3eeb61c463d479f12efabf13160512446c5edf64106f7481347a
+size 518
