5e6a27ae9b31f3db77ca90229475d6265e214ce8
최재성 <we0620@naver.com>
2024-04-22 15:11:36 +0900

    feat:TeamSpace

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,49 +1,51 @@
 import React, { useEffect } from "react";
-import app from './firebase'
+import app from "./firebase";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import LoginPage from "./loginPage/LoginPage";
 import RegisterPage from "./registerPage/RegisterPage";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useDispatch } from "react-redux";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { clearUser, setUser } from "./store/userSlice";
 import TestPage from "./testPage/TestPage";
 import CodeTestPage from "./testPage/CodeTestPage";
 import SurveyPage from "./surveyPage/SurveyPage";
-
+import TeamSpacePage from "./teamspacePage/TeamSpacePage";
+import "./fonts/Font.css"
 const App = () => {
-
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const auth = getAuth(app)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const auth = getAuth(app);
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if(user) {
-        navigate('/')
-        dispatch(setUser({
-          uid: user.uid,
-          displayName: user.displayName,
-        }))
+      if (user) {
+        navigate("/");
+        dispatch(
+          setUser({
+            uid: user.uid,
+            displayName: user.displayName,
+          })
+        );
+      } else {
+        navigate("/login");
+        dispatch(clearUser());
       }
-      else {
-        navigate('/login')
-        dispatch(clearUser())
-      }
-    })
+    });
     return () => {
-      unsubscribe()
-    }
-  }, [])
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <Routes>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path='/register' element={<RegisterPage/>}/>
-      <Route path='/' element={<TestPage/>}/>
-      <Route path='/codetest' element={<CodeTestPage/>}/>
-      <Route path='/survey' element={<SurveyPage/>}/>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/" element={<TestPage />} />
+      <Route path="/codetest" element={<CodeTestPage />} />
+      <Route path="/survey" element={<SurveyPage />} />
+      <Route path="/TeamSpacePage" element={<TeamSpacePage />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
