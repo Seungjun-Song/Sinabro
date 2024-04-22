cb089ae6e7799b27beb68ce706449be4df95833e
김윤민 <qkslzl101216@naver.com>
2024-04-22 15:06:43 +0900

    Init: 초기화
    초기화

--- be/src/main/java/com/HP50/be/domain/project/controller/ProjectController.java
+++ be/src/main/java/com/HP50/be/domain/project/controller/ProjectController.java
@@ -1,4 +1,20 @@
 package com.HP50.be.domain.project.controller;
 
+import com.HP50.be.global.common.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/teams")
 public class ProjectController {
+    @GetMapping
+    private ResponseEntity<Object> getTeamInfo(){
+        //ResponseEntity.status(HttpStatus.CREATED).body(new BaseResponse<>(StatusCode.SUCCESS));
+        return ResponseEntity.ok(new BaseResponse<>(null));
+    }
+
 }
---
+++ be/src/main/java/com/HP50/be/domain/project/entity/PjtTechStack.java
@@ -0,0 +1,27 @@
+package com.HP50.be.domain.project.entity;
+
+import com.HP50.be.domain.code.entity.SubCategory;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PjtTechStack {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer pjtTechStackId;
+
+    @ManyToOne
+    @JoinColumn(name = "teammate_id")
+    private Teammate teammate;
+
+    @ManyToOne
+    @JoinColumn(name = "subcategory_id")
+    private SubCategory subCategory;
+}
