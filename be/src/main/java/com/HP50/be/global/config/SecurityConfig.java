be969d57e678e1fd171882610d1e6adea43083da
JongKook <whdrnrdl789@naver.com>
2024-04-23 00:17:52 +0900

    Feat: Github Login Success
    Related to #4

--- be/src/main/java/com/HP50/be/global/config/SecurityConfig.java
+++ be/src/main/java/com/HP50/be/global/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.HP50.be.global.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -8,15 +10,20 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 
+@Configuration
 // Security를 활성화 하겠다.
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated()
-                ).oauth2Login(withDefaults());
-        return http.build();
+        return http
+                .authorizeHttpRequests(auth->
+                    auth.requestMatchers("/").permitAll()
+                            .anyRequest().authenticated()
+                )
+
+                .oauth2Login(withDefaults())
+                .build();
     }
 }
