485b643bae3acd0e8959d90b62356aefcbd6ccab
김윤민 <qkslzl101216@naver.com>
2024-04-22 16:55:40 +0900

    Refactor: 포트 수정
    포트 수정

--- be/src/main/java/com/HP50/be/domain/port/entity/Port.java
+++ be/src/main/java/com/HP50/be/domain/port/entity/Port.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:159784bfcb727a368197c0058ac387ad44fa068963c24d1949dcc6e088025452
-size 489
+oid sha256:4075667c76a8e12b35256f24d85b8e6d97fff7734e737366c36ffdf15fe14ce5
+size 519
