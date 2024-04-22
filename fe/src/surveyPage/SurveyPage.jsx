2d1414689af60001044f142016a2eb20f485201e
JeonEunHui <64572911+JeonEunHui@users.noreply.github.com>
2024-04-22 14:58:03 +0900

    feat: nav bar 생성 nav bar 중 팀 스페이스 클릭 시 프로젝트 목록이 보여짐. 아직은 틀로 실제 데이터를 가져와서 보여질 필요 있음
    Related to : #2

--- fe/src/surveyPage/SurveyPage.jsx
+++ fe/src/surveyPage/SurveyPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db0c03ab6deadd85f879dc9aeff108f0ee42857089f6db91db475f2da8cc64db
-size 1377
+oid sha256:d5dc59242bbf839e31a1be0fa65f6c0418afb17c8ee6765c44f723cb86b1fd87
+size 1461
