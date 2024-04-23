e6f14aa148a16b2632ce452bfa819ec034fd9f59
JeonEunHui <64572911+JeonEunHui@users.noreply.github.com>
2024-04-23 17:13:14 +0900

    style: 전 커밋 issue 연결 안 되서 다시 올리는 커밋입니다 이전 커밋 내용      feat: 커뮤니티 페이지 중 팀원 구해요 틀 완성     글쓰기 버튼 링크는 글쓰기 페이지가 없어서 존재하지 않음     옵션 토글의 경우 아직 작성글을 걸러내지 않음 Related to : #7

--- fe/src/communityPage/ProceedSelection.jsx
+++ fe/src/communityPage/ProceedSelection.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a81198e730b5d6e664c70de69ce1c77683abbad4a251d34b746a866b64234329
-size 979
+oid sha256:24df0353e17a793cfe0e3b88e3a6f6cb45489b8c0fdfc8b2d1803ed78e7c9a66
+size 980
