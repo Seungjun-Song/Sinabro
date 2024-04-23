c897e41571f8700499ef56b8fdc30cd199fcc960
JongKook <whdrnrdl789@naver.com>
2024-04-23 17:33:32 +0900

    Fix: Remove Sercurity Config

--- be/src/main/java/com/HP50/be/domain/memoryGraph/entity/Neo4jMember.java
+++ be/src/main/java/com/HP50/be/domain/memoryGraph/entity/Neo4jMember.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b339602b4e9f81856cbfbdb98dcb08134f096c8cd73f0ef19ce7d2625f14fc90
-size 603
+oid sha256:809b09ed9b0f6cde0c5606fdd091fce6191d53f098aacf2008d5c28551d8592b
+size 608
