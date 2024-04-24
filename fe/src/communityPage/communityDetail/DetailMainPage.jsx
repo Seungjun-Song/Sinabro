bdbbc244a3740a03849a9ca6943f27a521ca38f9
전은희 <jeon78eun@naver.com>
2024-04-24 15:27:10 +0900

    feat: 팀원 구해요 상세 페이지 팀원 구해요 상세 페이지 기본 틀 댓글은 아직 구현되지 않음
    Related to: #9

--- fe/src/communityPage/communityDetail/DetailMainPage.jsx
+++ fe/src/communityPage/communityDetail/DetailMainPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cfde5a180e54b383ac167427ba77de34a7eb53b1a3ced97ef2d7ea5c6169df8
-size 971
+oid sha256:84d6b91da41e5d28a44dddc454fc3e8ef9a66adff56470d8afdc89c13bfa905d
+size 1024
---
+++ fe/src/communityPage/communityDetail/DetailMember.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1015abaddc27c1eaac093e1e2ccad675d3cf4a6dd156aff458edb2c5b68e86b9
+size 5755
