9b51dd263491af8357614f5878c4880ee7472d85
JeonEunHui <64572911+JeonEunHui@users.noreply.github.com>
2024-04-24 10:53:53 +0900

    feat: 팀원 구해요 글 생성 페이지 팀원 구해요 글 생성 페이지 완성. 취소/생성 버튼시 다시 목록으로 돌아감. Todo: 생성 버튼 클릭 시 데이터 저장 로직 Related to : #9

--- fe/src/communityPage/createPost/CreateMemberPost.jsx
+++ fe/src/communityPage/createPost/CreateMemberPost.jsx
@@ -1,12 +1,3 @@
-import styled from 'styled-components'
-
-const CreateMemberPost = () => {
-
-    return(
-        <>
-            
-        </>
-    )
-}
-
-export default CreateMemberPost;
+version https://git-lfs.github.com/spec/v1
+oid sha256:d5986d35a604efb389a2411e5ea3c23efa15140b56011c4184feae3a4ac55486
+size 3914
