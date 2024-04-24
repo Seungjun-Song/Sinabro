69b811263472ec3850c647120cb51d9c306cba18
최재성 <we0620@naver.com>
2024-04-24 14:56:20 +0900

    FE:팀 스페이스 상세보기 페이지

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7521b79aa3dd754eee3c7cecc45aa333c2a06d989d7678e056619bcfa04edf50
-size 2143
+oid sha256:049f7c5c03bafb702c6fcd371adbf96c3d78e4bb726a6eefc6766391eb6fcc7c
+size 2101
