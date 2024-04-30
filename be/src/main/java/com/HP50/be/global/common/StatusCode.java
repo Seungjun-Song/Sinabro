553c229feb252e04117b479f08782a09bad44f99
박종국 <whdrnrdl789@naver.com>
2024-04-30 23:10:33 +0900

    Fix: Delete Duplicate StatusCode

--- be/src/main/java/com/HP50/be/global/common/StatusCode.java
+++ be/src/main/java/com/HP50/be/global/common/StatusCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c0b62ccf611da11d2ef28be96c4a2b9a7d4e019337cbba99e309cb0140c3851
-size 3074
+oid sha256:9f39b32d00a9d39d69ae00b51c1ac5306bfeb58316c6d1b09ba299923fcf1269
+size 2747
