16c57f689fc9988502835a5ea7dd0e35464e02b3
박종국 <whdrnrdl789@naver.com>
2024-05-01 20:42:04 +0900

    Feat: SuccessUrl fix

--- be/src/main/java/com/HP50/be/global/config/SecurityConfig.java
+++ be/src/main/java/com/HP50/be/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07d3fc12ac3a24c729527dfd8735d52a604c00d5abeb6626ad9ada4ca280848d
-size 2876
+oid sha256:34359ab297189289a5eab8fdc3c7f69fcb3f56aaf5c0641897232f8a38f359a8
+size 2787
