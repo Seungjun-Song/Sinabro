50b8b72284865f38a5602244d79c6adc175228cd
전은희 <jeon78eun@naver.com>
2024-05-02 11:14:40 +0900

    Design: 커뮤니티 다크모드 설정 추가 커뮤니티 다크모드 설정 추가 Related to : #14

--- fe/src/services/color.jsx
+++ fe/src/services/color.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45e2d54888aff7cff1e3b0f5ee873085c225c75e550ecf4442da9cc896bc4952
+oid sha256:aea75c05215a051ab89f79b261c82bf32ebb2ad22f44b8ae92faf8c00227cf52
 size 1444
