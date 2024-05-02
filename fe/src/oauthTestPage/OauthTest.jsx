411fa76d8606adb852946110491ae1a4f1660bce
전은희 <jeon78eun@naver.com>
2024-05-02 17:12:59 +0900

    Fix: testPage Oauth링크 수정
    testPage Oauth 링크 수정
    
    Related to : #18

--- fe/src/oauthTestPage/OauthTest.jsx
+++ fe/src/oauthTestPage/OauthTest.jsx
@@ -13,16 +13,19 @@ const OauthTest = () => {
 
     const useAxios = () => {
         // axios.get('http://localhost:8080/api/oauth2/authorization/github')
-        axios.get(`https://k10e103.p.ssafy.io/api/oauth2/authorization/github`)
-        .then((res) => {
-            //setCookie(res);
-            console.log("cookie");
-            console.log(JSON.stringify(res));
+        // axios.get(`https://k10e103.p.ssafy.io/api/oauth2/authorization/github`)
+        // .then((res) => {
+        //     //setCookie(res);
+        //     console.log("cookie");
+        //     console.log(JSON.stringify(res));
             
-        })
-        .catch((err) => {
+        // })
+        // .catch((err) => {
 
-        });
+        // });
+
+        const redirectUrl = 'https://k10e103.p.ssafy.io'
+        
     }
 
     return (
