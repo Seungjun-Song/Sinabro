5610733a127b2b4b0f7ec22779e8885ec4841d14
김윤민 <qkslzl101216@naver.com>
2024-05-02 12:48:00 +0900

    Refactor: cors 에러 잠깐 fix

--- be/src/main/java/com/HP50/be/domain/JwtTestController.java
+++ be/src/main/java/com/HP50/be/domain/JwtTestController.java
@@ -1,28 +1,3 @@
-package com.HP50.be.domain;
-
-import com.HP50.be.global.jwt.JwtUtil;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/jwt")
-@RequiredArgsConstructor
-public class JwtTestController {
-    private final JwtUtil jwtUtil;
-    @GetMapping
-    public String redirectResponse(@RequestParam("accessToken") String accessToken,
-                                   @RequestParam("refreshToken") String refreshToken){
-        return "<h1> 정상적으로 작동 되었습니다.</h1>" +
-                " " + accessToken +
-                " " + refreshToken;
-    }
-    
-    @GetMapping("/test")
-    public String redirectResponse1(String token){
-        Integer memberId = jwtUtil.getMemberId(token);
-        return "<h1> test로 무사히 이동완료.<h1>";
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:70d30bf9c0d03d0819c77dac3af0c744eb24fa4db0ad4199e65ca5ea9e807e56
+size 1024
--- be/src/main/java/com/HP50/be/global/jwt/JwtConstants.java
+++ be/src/main/java/com/HP50/be/global/jwt/JwtConstants.java
@@ -1,3 +1,19 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6b65f2fc867413dd533817bd581d790ebe7dd28de5c48cb3589371f16537e84a
-size 755
+package com.HP50.be.global.jwt;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
+// 기본 생성자를 가지지만 외부에서는 접근할 수 없음
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class JwtConstants {
+    public static final String COMPANY = "HP50";
+    public static final long ACCESS_EXP_TIME = 60000 * 60;   // 1시간 분 설정
+    public static final long REFRESH_EXP_TIME = 60000 * 60 * 24;   // 24시간 설정
+
+    public static final String JWT_HEADER = "Authorization";
+    public static final String JWT_TYPE = "BEARER ";
+
+    public static final String ACCESS = "access_token ";
+    public static final String REFRESH = "refresh_token ";
+}
