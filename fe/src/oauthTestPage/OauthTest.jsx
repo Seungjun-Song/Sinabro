3c42e066580a62121fc098e1ca7b7e3cbbb80e6a
전은희 <jeon78eun@naver.com>
2024-05-02 16:20:04 +0900

    Feat: oauth연결 테스트
    로컬에서는 안 되는데 배포 서버에서 되는지 확인
    
    Related to : #18

---
+++ fe/src/oauthTestPage/OauthTest.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cf3b66b532fc1a315f5da233d2aa5e4e26552075b13d4f3b07bd461fd91807cf
+size 1000
--- fe/src/testPage/TestPage.jsx
+++ fe/src/testPage/TestPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34c6314f581ae2989e5a4498bbd8759b8986d68bec264ff9909c92f2f932374e
-size 2301
+oid sha256:d2fb392fc45e3c36c7226cc72d71e1be8ded717c86c0ad8ea82445c1fa3ef62c
+size 2551
