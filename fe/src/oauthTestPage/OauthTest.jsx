10749be5ceb321df4f7cb0acccef5b84d5fd1456
전은희 <jeon78eun@naver.com>
2024-05-02 16:40:18 +0900

    Fix: react오류
    react객체 오류 수정
    
    Related to : #18

--- fe/src/oauthTestPage/OauthTest.jsx
+++ fe/src/oauthTestPage/OauthTest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f5795b0b7fb18e546c248931b1fcbb0d26279d459fabe794eaa1c87ad8c35e0
-size 1029
+oid sha256:e1a36df8aabd5dabe2cbcf922163142c32619c252f430d89f76cd42237abddc6
+size 1031
