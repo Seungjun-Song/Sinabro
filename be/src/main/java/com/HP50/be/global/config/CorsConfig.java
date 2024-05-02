323a2c0abdd3f5c16b91b63c7a33fd90274f533a
박종국 <whdrnrdl789@naver.com>
2024-05-02 11:14:28 +0900

    Feat: AllowCredential true again

--- be/src/main/java/com/HP50/be/global/config/CorsConfig.java
+++ be/src/main/java/com/HP50/be/global/config/CorsConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3740901507f30f11878ba3e530aa58f7f8c3514860ab2346d3f91cd89242bd32
-size 965
+oid sha256:c7c35609150b5be7fa96e228dde7bc5e4eb2f3ae5b8db0e68d9f9ff132dbec02
+size 1337
