3d1a045568da4013400a8fbed88fd9127ab19fa7
박종국 <whdrnrdl789@naver.com>
2024-05-02 12:39:07 +0900

    Feat: sendRedirect to localhost

--- be/src/main/java/com/HP50/be/global/oauth/handler/CustomSuccessHandler.java
+++ be/src/main/java/com/HP50/be/global/oauth/handler/CustomSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75ee0e9b58fcd39a896a6c51038ffd7d2865cc93c1e0b70782200188a494e821
-size 3136
+oid sha256:dc8812a29ecbaabb270c6c449278573dd531793f30c45e3d68d7f75b0bee7aaf
+size 3131
