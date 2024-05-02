1ee3bb55bc96ad4b767e65719a51fffd90c5fcf3
김윤민 <qkslzl101216@naver.com>
2024-05-02 14:53:14 +0900

    Revert "Refactor: cors 에러 잠깐 fix"
    This reverts commit 66777c37fe708a44a148e426d75e90c4d8527903.

--- be/src/main/java/com/HP50/be/global/config/CorsConfig.java
+++ be/src/main/java/com/HP50/be/global/config/CorsConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7843ccca71d0106715565a1affcca15ff8b1969815fbab31941cd326818cf61
-size 1190
+oid sha256:6201e35d3117cc59e876477bafa0b935e0796b1daf731f3ca948cc3f61a8a9e9
+size 1473
