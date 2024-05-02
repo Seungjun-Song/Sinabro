66777c37fe708a44a148e426d75e90c4d8527903
김윤민 <qkslzl101216@naver.com>
2024-05-02 12:48:00 +0900

    Refactor: cors 에러 잠깐 fix

--- be/src/main/java/com/HP50/be/global/config/SecurityConfig.java
+++ be/src/main/java/com/HP50/be/global/config/SecurityConfig.java
@@ -5,15 +5,23 @@ import com.HP50.be.global.jwt.JwtUtil;
 import com.HP50.be.global.oauth.CustomOAuth2UserService;
 import com.HP50.be.global.oauth.handler.CustomFailureHandler;
 import com.HP50.be.global.oauth.handler.CustomSuccessHandler;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 // Security를 활성화 하겠다.
@@ -31,11 +39,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 // .csrf(csrf -> csrf.disabled()) 같은 표현
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .addFilterBefore(corsConfig.corsFilter(), UsernamePasswordAuthenticationFilter.class)
-
+                .csrf(CsrfConfigurer::disable)
+                .cors(cors -> cors
+                        .configurationSource(corsConfig.configurationSource())
+                )
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
                 )
