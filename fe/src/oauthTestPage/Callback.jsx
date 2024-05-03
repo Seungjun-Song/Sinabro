4870b2adb9de8e944dfdf51052f8d1209c03435d
전은희 <jeon78eun@naver.com>
2024-05-03 12:42:32 +0900

    Fix: 로그인 callback 주소 수정
    callback 주소 수정
    
    Related to : #18

--- fe/src/oauthTestPage/Callback.jsx
+++ fe/src/oauthTestPage/Callback.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61194ccc10968fe9df2c1b149e7798f54c975e44181e53a82510cf552d4494f5
-size 518
+oid sha256:0e0f1dd85e2c6ccf906722934aad2ec66bd0de8460152db962ccb9b7b35cc680
+size 547
--- fe/src/oauthTestPage/OauthTest.jsx
+++ fe/src/oauthTestPage/OauthTest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb3269b6e39e2ce34b035bd15fecfa01286694a788f639b5e9f640c73f4b7cd8
+oid sha256:05d78c3ab13fba6abb67088d89caa9e45dbddea9642f84409f6f92d020f5f420
 size 2291
