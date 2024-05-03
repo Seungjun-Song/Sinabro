c209611922d421f32ea12c18e5a6eb010b7b0307
전은희 <jeon78eun@naver.com>
2024-05-03 12:29:17 +0900

    Fix: 로그인 code
    callback 주소 변경
    
    Related to: #18

--- fe/src/oauthTestPage/OauthTest.jsx
+++ fe/src/oauthTestPage/OauthTest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1695b22222225733552daa39679c1b8d6793205a4c8f90cfabfd9950f4261d96
-size 2256
+oid sha256:bb3269b6e39e2ce34b035bd15fecfa01286694a788f639b5e9f640c73f4b7cd8
+size 2291
