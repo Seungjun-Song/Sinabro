f03e5b911b1e9c2d9fb10b59415d95b851b18119
박종국 <whdrnrdl789@naver.com>
2024-05-08 01:58:13 +0900

    Feat: Pagination Test

--- be/src/main/java/com/HP50/be/domain/community/controller/BoardController.java
+++ be/src/main/java/com/HP50/be/domain/community/controller/BoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:767b45b5f0b41d7c74e0160d30e82bd24c90f8a3a5f63422ad9a60bbe50d2105
-size 2245
+oid sha256:5f21370206c466feec8f96e72f500976a4065f856fa50524829860ee0a3a19a4
+size 2363
--- be/src/main/java/com/HP50/be/domain/community/service/BoardService.java
+++ be/src/main/java/com/HP50/be/domain/community/service/BoardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9693b28665a4f596ef3bc67444ce43df20cf2489b011a87abdac4d49bc81609f
-size 917
+oid sha256:f29318ebc1324e67f1a0eb164bf7648f683247b360106ff3368945caf5b70d8a
+size 976
