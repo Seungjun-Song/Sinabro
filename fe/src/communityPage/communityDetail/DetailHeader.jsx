1095d0e0054397601a1251020da07750dba536ee
전은희 <jeon78eun@naver.com>
2024-05-07 16:46:43 +0900

    Feat: 게시글 삭제
    게시글 삭제 기능
    
    Related to: #22

--- fe/src/communityPage/communityDetail/DetailHeader.jsx
+++ fe/src/communityPage/communityDetail/DetailHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77438578e65ed138d730e86087d257f40e86a9a5b770d0e8f25bb59b9cf2a85f
-size 1999
+oid sha256:8820dc378d92026d8fb853d4fe92aabd9b077d329489c1f71db9304b881595ed
+size 2894
