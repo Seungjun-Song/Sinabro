e63ea8468a284404e97e8b79ceeadc8d1f7f00b8
전은희 <jeon78eun@naver.com>
2024-05-07 17:26:57 +0900

    Feat: 커뮤니티 게시글 수정
    Related to : #22

--- fe/src/communityPage/communityDetail/DetailHeader.jsx
+++ fe/src/communityPage/communityDetail/DetailHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8820dc378d92026d8fb853d4fe92aabd9b077d329489c1f71db9304b881595ed
-size 2894
+oid sha256:2de9efefb7989477d507619c7beff35708fa4c215c8875115cb77ccd239977f3
+size 3554
