fa3b1d68197c220cc00facdd939d0387c356a4b8
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-08 17:45:31 +0900

    Fix: project page update
    Project page update
    
    Related to #6

--- fe/src/components/teamspaceComponents/ProjectCreateBtn.jsx
+++ fe/src/components/teamspaceComponents/ProjectCreateBtn.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7b3dc637c2f30d83b8c5107940b60aa5978d9e6cd136b0c13367b05e99bb45b
-size 1516
+oid sha256:07d161691afc6bc10c54079516c6cd97f3c72795a74b2bc8f4b3ed04f0090f4d
+size 1633
--- fe/src/projectPage/ProjectPageLeftPanel.jsx
+++ fe/src/projectPage/ProjectPageLeftPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84a621e5efe7b8168d0a703e5eec569d9fe1cd0e21dfbd2b646f2c37a34f64ab
-size 21656
+oid sha256:2cc7de40994f4143d53bc59327808004e3f53006ae762f549bdcab1c9323cc3d
+size 21661
