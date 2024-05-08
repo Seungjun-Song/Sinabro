a02709a55c422242e047d76083c07e7a33ea3826
박종국 <whdrnrdl789@naver.com>
2024-05-08 16:19:56 +0900

    Feat: memberPK & CommentPK 추가

--- be/src/main/java/com/HP50/be/domain/community/service/BoardServiceImpl.java
+++ be/src/main/java/com/HP50/be/domain/community/service/BoardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41a24e4cf73d2b30e1efbad6fff3e94a09c734bc64805e81a9dc954f7ba0eb86
-size 7287
+oid sha256:bbe3e5dff90cae8e0c8b95a3426a1fc0a4f7288e2b05eb6f4a734f007a2e2f35
+size 7346
--- be/src/main/java/com/HP50/be/domain/member/dto/TechStackSaveRequestDto.java
+++ be/src/main/java/com/HP50/be/domain/member/dto/TechStackSaveRequestDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bccab0e375765d3cae6b542ce3da820e35782c75f3f8aa6f2a45d5dd500bee4
-size 205
+oid sha256:8d1f0d195a7d4e73d6694fcc14d2206ad35f5660a0792a1cfc3371172230b924
+size 361
