67e446c798dc9f84ddbe857d273dbc38d547ac31
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-08 16:07:09 +0900

    Fix: survey page update
    Survey page update
    
    Related to #1

--- fe/src/communityPage/createPost/CkEditor.jsx
+++ fe/src/communityPage/createPost/CkEditor.jsx
@@ -1,3 +1,103 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d1faa994a794b7d4473752fb3c586a1db416602b8ba86ef734d69adfb25e5aa2
-size 2104
+import styled, { css } from 'styled-components'
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+import { GlobalColor } from '../../services/color';
+
+const StyledEditor = styled.div`
+  width: 100%; // CKEditor 컨테이너 너비를 100%로 설정
+  .ck.ck-editor {
+    width: 100%; // 에디터 자체의 너비를 100%로 설정
+    margin: auto;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    background-color: #f8f8f8;
+  }
+
+  .ck.ck-content {
+    min-height: 500px;
+  }
+
+  ${props => props.isDark && css`
+  	.ck.ck-editor{
+
+	}
+  	.ck.ck-content{
+		background-color: ${GlobalColor.colors.ckEditor_back_dark};
+	}
+
+	.ck.ck-toolbar {
+		background-color: ${GlobalColor.colors.ckEditor_back_dark};// 툴바 배경색 변경
+	}
+
+  `}
+`;
+
+const CkEditor = ({ isDark, postContent, setPostContent }) => {
+
+    const edrtorConfiguration = {
+		toolbar: {
+			items: [
+				'heading',
+				'|',
+				'bold',
+				'italic',
+				'link',
+				'bulletedList',
+				'numberedList',
+				'|',
+				'outdent',
+				'indent',
+				'|',
+				'blockQuote',
+				'undo',
+				'redo'
+			]
+		},
+		language: 'ko',
+		image: {
+			toolbar: [
+				'imageTextAlternative',
+				'toggleImageCaption',
+				'imageStyle:inline',
+				'imageStyle:block',
+				'imageStyle:side',
+				'linkImage'
+			]
+		},
+		table: {
+			contentToolbar: [
+				'tableColumn',
+				'tableRow',
+				'mergeTableCells',
+				'tableCellProperties',
+				'tableProperties'
+			]
+		}
+    };
+
+    return(
+        <StyledEditor
+			isDark={isDark}
+		>
+        <CKEditor
+            editor={ClassicEditor}
+            config={edrtorConfiguration}
+            placeholder={"프로젝트를 설명해 주세요!"}
+            
+            onReady={editor => {
+                // You can store the "editor" and use when it is needed.
+				editor.setData(postContent.content);
+            }}
+            onChange={(event, editor) => {
+				setPostContent((prevState) => {
+					return {...prevState, content: editor.getData()};
+				})
+            }}
+        />
+
+        </StyledEditor>
+    )
+}
+
+export default CkEditor;
--- fe/src/communityPage/createPost/CreatePage.jsx
+++ fe/src/communityPage/createPost/CreatePage.jsx
@@ -1,3 +1,159 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:08f1e6a4340c99aa2ac912ff8d213629b2d76b79c6b34b7ea95f59a15a6195d1
-size 4232
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import styled, { css } from 'styled-components'
+import { motion } from "framer-motion"
+import { useSelector } from "react-redux";
+import { Modal } from 'react-bootstrap';
+
+import Navbar from '../../components/navs/Navbar';
+import CreateMemberPost from './CreateMemberPost';
+import CreateTeamPost from './CreateTeamPost';
+import CreateFeadbackPost from './CreateFeadbackPost';
+import TeamChoiceBox from './TeamChoiceBox';
+
+import { GlobalColor } from '../../services/color';
+
+const Back = styled.div`
+    height: 100%;
+
+    ${props => props.isDark && css`
+        background: ${GlobalColor.colors.primary_black};
+    `}
+`
+const Create = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 65%;
+    margin: 4rem auto 0 auto;
+`
+const Header = styled(motion.div)`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+    
+`
+
+const Options = styled.div`
+    display: flex;
+    align-items: start;
+    justify-content: start;
+    gap: 1rem;
+
+    margin: 3rem;
+
+    width:100%;
+`
+
+const Option = styled.div`
+
+    padding: 0.2rem 1rem;
+    
+    background: rgba(240, 240, 240, 1);
+    color: rgba(86, 76, 173, 1);
+
+    cursor: pointer; 
+    
+    font-family: Jamsil Regular;
+
+    border: 0px solid black;
+    border-radius: 10px;
+
+    ${props => props.selected && css`
+        background: rgba(86, 76, 173, 1);
+        color: rgba(240, 240, 240, 1);
+    `}
+`
+
+const headerMotion = {
+    initial: "hidden",
+    animate: "visible",
+    exit: "hidden",
+    variants: {
+        hidden: { opacity: 0, y: -50 },
+        visible: { opacity: 1, y: 0 }
+    },
+    transition: { duration: 0.3 }
+}
+
+const CreatePage = () => {
+    const location = useLocation();
+    const data = location.state;
+
+    const [ selected, setSelected ] = useState(data.kind);
+    const [ postContent, setPostContent ] = useState({
+        title: '',
+        content: '',
+        tag: ''
+    });
+
+    const isdark = useSelector(state =>state.isDark.isDark);
+
+    const changeOption = (option) => {
+        setSelected(option);
+    }
+
+
+
+    return(
+        <>
+            <Navbar>
+            </Navbar>
+            <Back style={{transition:"0.3s"}} isDark={isdark}>
+            <Create
+                isDark={isdark}
+            >
+                <Header
+                    {...headerMotion}
+                >
+                    <Options>
+                    <Option onClick={() => changeOption("member")}
+                        selected={selected === "member"}>
+                        팀원 구해요
+                    </Option>
+                    <Option onClick={() => changeOption("team")}
+                        selected={selected === "team"}>
+                        팀 구해요
+                    </Option>
+                    <Option onClick={() => changeOption("feadback")}
+                        selected={selected === "feadback"}>
+                        피드백 원해요
+                    </Option>
+                    </Options>
+                    {selected === "member" || selected === "feadback" ? (
+                        <TeamChoiceBox/>
+                    ) : ("")}
+                </Header>
+
+                {selected === "member" ? (
+                    <CreateMemberPost
+                        isdark={isdark}
+                        postContent={postContent}
+                        setPostContent={setPostContent}
+                    />
+                ) : ("")}
+                {selected === "team" ? (
+                    <CreateTeamPost
+                        isdark={isdark}
+                        postContent={postContent}
+                        setPostContent={setPostContent}
+                        
+                    />
+                ) : ("")}
+                {selected === "feadback" ? (
+                    <CreateFeadbackPost
+                        isdark={isdark}
+                        postContent={postContent}
+                        setPostContent={setPostContent}
+                    />
+                ) : ("")}
+            </Create>
+            </Back>
+        </>
+    )
+}
+
+export default CreatePage;
