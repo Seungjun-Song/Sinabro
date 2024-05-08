34ceeef583e797fc751593c1a604124c0fea2250
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-08 17:51:58 +0900

    Fix: project page update
    Project page update
    
    Related to #6

--- fe/src/projectPage/ProjectPageLeftPanel.jsx
+++ fe/src/projectPage/ProjectPageLeftPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cc7de40994f4143d53bc59327808004e3f53006ae762f549bdcab1c9323cc3d
-size 21661
+oid sha256:3fa789999a2bb64d7a146e354b9706277d67f46a3ab316b9882d35bb78d5a111
+size 21757
