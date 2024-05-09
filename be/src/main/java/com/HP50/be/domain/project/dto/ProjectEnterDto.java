d5592c488986472b87c982a41c98cd41db42dfd1
박종국 <whdrnrdl789@naver.com>
2024-05-10 00:42:55 +0900

    Feat: logout

--- be/src/main/java/com/HP50/be/domain/project/dto/ProjectEnterDto.java
+++ be/src/main/java/com/HP50/be/domain/project/dto/ProjectEnterDto.java
@@ -1,11 +1,3 @@
-package com.HP50.be.domain.project.dto;
-
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-public class ProjectEnterDto {
-    private String url;
-    private Integer dbPort;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:8828ec4f044c0c7d90b69222efa6e73281c2b03cfc7fe62f40db3cb800acc442
+size 189
