76314fa3c781558020ffcb7506cf9859cb2ef37a
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-09 16:40:49 +0900

    Fix: team space code detail page fix
    Team space code detail page fix
    
    Related to #21

--- fe/src/components/TeamSpaceDetailComponent/ProjectTeam.jsx
+++ fe/src/components/TeamSpaceDetailComponent/ProjectTeam.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d78108a0f2290aba125fe5b40e5dc8228b5253657d3bc96e5fc5c14fb146402f
-size 3767
+oid sha256:e17c0123470a7df90385e2da82a15f5a9a563a3a3eee91a97dbd0955ef3153bb
+size 3774
