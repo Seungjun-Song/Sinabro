0b93256001e3681023c00bd6c05d96023cff5c46
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-09 12:27:12 +0900

    Fix: project page api fix
    project page api fix
    
    Related to #6

--- fe/src/components/TeamSpaceDetailComponent/Todo.jsx
+++ fe/src/components/TeamSpaceDetailComponent/Todo.jsx
@@ -1,16 +1,3 @@
-const day_list = [ "Today", "D+1", "D+2", "D+3","D+4","D+5","D+6","D+7"];
-const Todo = () => {
-  return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <div>이번주 할것!</div>
-      <div style={{ display: "flex", width: "100%", height: "100%" }}>
-        {day_list.map((item) => (
-          <div style={{ width: "14%", height: "100%", textAlign: "center" }}>
-            {item}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-export default Todo;
+version https://git-lfs.github.com/spec/v1
+oid sha256:89c722ef0edba72dd9b92fec1aaaa36ac0cf3d1ea2c8fbcd3d95bd315853cb1a
+size 477
