4c962d663e680ea24471c7f7ef2b4f5b4ec933d5
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-09 18:04:34 +0900

    Fix: team space page fix
    team space page fix
    
    Related to #21

--- fe/src/components/teamspaceComponents/ProjectGitConnect.jsx
+++ fe/src/components/teamspaceComponents/ProjectGitConnect.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a8a1e20b17d004e43dc440ea493edb63d611476bf088a4fede5d654a3da8ef4
-size 7467
+oid sha256:f018bd78efe981a3479afba9cc872c50b4a97f7ea07a8bf4c921f5a7b3130fc9
+size 7437
