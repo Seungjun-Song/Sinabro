bd2d8b1bc689df38a70d5c9265bd57cbb99153fa
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-09 17:50:50 +0900

    Fix: project info fix
    project info fix
    
    Related to #221

--- fe/src/components/TeamSpaceDetailComponent/Projectexplanation.jsx
+++ fe/src/components/TeamSpaceDetailComponent/Projectexplanation.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:578f68fb722cc3ab363add37194413453fd4dc63b8e71e43477913d8582247fb
-size 995
+oid sha256:65bed7fa3adf4adf7f6229a64bb8561ea127dcdf198a2781772a94356c22bdf4
+size 1092
