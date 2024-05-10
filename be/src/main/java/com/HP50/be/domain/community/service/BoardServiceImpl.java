b2dde94cdf7fb6691637c1f0d8e29c757fcb4841
박종국 <whdrnrdl789@naver.com>
2024-05-10 11:34:47 +0900

    Feat: Board's projectId

--- be/src/main/java/com/HP50/be/domain/community/service/BoardServiceImpl.java
+++ be/src/main/java/com/HP50/be/domain/community/service/BoardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b29193e5a6338424a279ebc6e87705fc20ab387fdfd6c658b36d7b8426a003dc
-size 8695
+oid sha256:b0fd580f1c1bc1df0ec1fcc38f79c98991f89271cdd1f832cca63b9b62b78a37
+size 8697
--- be/src/main/java/com/HP50/be/domain/project/repository/PjtTechStackCustomRepositoryImpl.java
+++ be/src/main/java/com/HP50/be/domain/project/repository/PjtTechStackCustomRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7cca472919fecd8c5c47839b19ed09185d6b8d45997a97b959720d7caf17e91
-size 1314
+oid sha256:d2c7de07aad3f84052965ea90fde997213f550dd0ef3f57fe6f6c82c4bc1887d
+size 1476
