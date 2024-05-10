1882550cfa4bd2fd2863ffce677d59cf353eb2e8
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-10 12:49:01 +0900

    Feat: page connnect test
    page connnect test
    
    Related to #1

--- fe/src/components/boarding/Section1.jsx
+++ fe/src/components/boarding/Section1.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8891a269ac40c549ddbef93adec6f2b87ae67ca1558b3c44c3151853a9ba7ab7
-size 6825
+oid sha256:649a339a969ecdbf9880c8e48dc8cf1849ba74616140ae073c9b69eb9a748ebf
+size 6975
--- fe/src/oauthTestPage/Callback.jsx
+++ fe/src/oauthTestPage/Callback.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbeb947a500cb797441eae875d535368ae305dd77386f51a8f196337e51c8c17
-size 2385
+oid sha256:ecc3b881848c31ba0dac34910a36ff1dc7627bf9c7649101efe01bb35dc14aa4
+size 2448
