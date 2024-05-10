744697fa650cb5e01584bc67a130b8d08c6626e1
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-10 13:30:26 +0900

    Feat: page connnect test
    page connnect test
    
    Related to #1

--- fe/src/communityPage/createPost/CreateJobsBox.jsx
+++ fe/src/communityPage/createPost/CreateJobsBox.jsx
@@ -1,3 +1,133 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4f5c172c199a4d9620b5746cc01b9899847867fd5581e00208fa64d405c13051
-size 3491
+import styled, { css } from 'styled-components'
+import { faDesktop, faCog, faLeaf } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+
+const JobBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: start;
+
+    gap: 0.5rem;
+`
+
+const Job = styled.div`
+    border: 3px solid ${props => props.borderColor};
+    color: ${props => props.borderColor};
+
+    padding: 0.5rem 0.5rem;
+    gap: 10px;
+    border-radius: 5px;
+    max-height: 5rem;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    box-shadow: 3px 4px 3px rgba(217, 217, 217, 1);
+    font-size: 0.8rem;
+
+    cursor: pointer;
+
+    ${props => props.selected && css`
+        background: ${props => props.borderColor};
+        color: white;
+    `}
+
+`
+
+const InputBox = styled.input`
+    width: 0.5rem;
+    border: none;
+    padding: 0;
+
+`
+
+const CreateJobsBox = ({kind, jobInfo, setJobInfo}) => {
+
+    const selectJob = (job, index) =>{
+       
+        setJobInfo(jobInfo.map((v, i)=>{
+            if( index !== i){
+                return v;
+            }
+            else{
+                return {...v, selected: 1-job.selected}
+            }
+        }))
+    }
+
+    const changeTargetVal = (data, index) => {
+        setJobInfo(jobInfo.map((v, i) => {
+            if(index !== i ){
+                return v;
+            }
+            else{
+                if(data.nativeEvent.data == null)
+                    return {...v, target: ""}
+                else
+                    return{...v, target: data.nativeEvent.data};
+            }
+        }))
+    
+
+    }
+
+    const changeTotalVal = (data, index) => {
+        setJobInfo(jobInfo.map((v, i)=>{
+            if(index !== i){
+                return v;
+            }
+            else{
+                return{...v, total: data.nativeEvent.data}
+            }
+        }))
+
+    }
+    return(
+        <JobBox>
+            {kind.name == "member" && 
+            <>
+                {jobInfo.map((job, index) => (
+                <Job
+                    borderColor={job.borderColor}
+                >
+                    {job.icon && <FontAwesomeIcon icon={job.icon} style={{ fontSize: '15px' }} />}
+                    <div>
+                    {job.name}
+                    </div>
+                    <InputBox
+                        value={job.total}
+                        onChange={(data) => changeTotalVal(data, index)}
+                    >
+                    </InputBox> / 
+                    <InputBox
+                        value={job.target}
+                        onChange={(data) => changeTargetVal(data, index)}
+                    >
+                    </InputBox>
+                </Job>
+            ))}               
+            6명 이하 인원만 가능해요!
+            </>}
+            
+            {kind.name == "feadback" &&
+            <>
+                {jobInfo.map((job, index) => (
+                <Job
+                    borderColor={job.borderColor}
+                    selected={job.selected === 1}
+                    onClick={() => selectJob(job, index)}
+                >
+                    {job.icon && <FontAwesomeIcon icon={job.icon} style={{ fontSize: '10px' }} />}
+                    <div>
+                    {job.name}
+                    </div>
+                </Job>
+                ))}   
+            </>
+            }
+        
+        </JobBox>
+    )
+}
+
+export default CreateJobsBox;
--- fe/src/components/TeamSpaceDetailComponent/ProjectTeam.jsx
+++ fe/src/components/TeamSpaceDetailComponent/ProjectTeam.jsx
@@ -1,3 +1,146 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3a4d787a42de83727cbf0f5d8c744051a6dfb5d5787f238c35f9386dee04caa0
-size 3790
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+import WhatPos from "./WhatPos";
+import WhatPosCard from "./WhatPosCard";
+import FakeWhatPos from "./FakeWhatPos";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import getEnv from "../../utils/getEnv";
+
+const DUMMY_DATA = [
+  {
+    id: 1,
+    job: "FE",
+    state: "Reader",
+    name: "Juhun0283",
+  },
+  {
+    id: 2,
+    job: "FE",
+    state: "Member",
+    name: "Juhun0283",
+  },
+  {
+    id: 3,
+    job: "BE",
+    state: "Member",
+    name: "Juhun0283",
+  },
+  {
+    id: 4,
+    job: "FULL",
+    state: "Member",
+    name: "Juhun0283",
+  },
+];
+
+const ProjectTeam = ({ setWhatUser, isDark }) => {
+  const displayedRoles = [];
+
+  const myCurrentProject = useSelector(state => state.myCurrentProject.value)
+  const back_url = getEnv('BACK_URL')
+  
+  const [teamInfo, setTeamInfo] = useState([])
+
+  useEffect(() => {
+    const getProjectInfo = async () => {
+      try {
+        const res = await axios.get(`${back_url}/teams?projectId=${myCurrentProject.projectId}`)
+        console.log(res.data)
+        const transformedTeamInfo = res.data.result.teammateInfoList?.map(item => ({...item, teammateRole: convertTeammateRole(item.teammateRole)}))
+        console.log(transformedTeamInfo)
+        setTeamInfo(transformedTeamInfo)
+      }
+      catch (err) {
+        console.error(err)
+      }
+    }
+    getProjectInfo()
+  }, [myCurrentProject])
+
+  const convertTeammateRole = (originalRole) => {
+    switch (originalRole) {
+      case "프론트엔드":
+        return "FE";
+      case "백엔드":
+        return "BE";
+      case "풀스택":
+        return "FULL";
+      default:
+        return originalRole;
+    }
+  }
+
+  return (
+    <motion.div
+      style={{ display: "flex", gap: "1.5rem", height: "100%" }}
+      initial="hidden"
+      animate="visible"
+      variants={{
+        hidden: {},
+        visible: {
+          transition: {
+            staggerChildren: 0.1,
+          },
+        },
+      }}
+    >
+      {teamInfo.map((item, index) => {
+        if (!displayedRoles.includes(item.teammateRole)) {
+          displayedRoles.push(item.teammateRole); // 역할을 추가합니다.
+          return (
+            <motion.div
+              key={index}
+              style={{
+                display: "flex",
+                gap: "1rem",
+                flexDirection: "column",
+                height: "100%",
+              }}
+              variants={{
+                hidden: { opacity: 0, y: 20 },
+                visible: { opacity: 1, y: 0 },
+              }}
+            >
+              <WhatPos item={item.teammateRole} isDark={isDark} />
+              <WhatPosCard
+                isDark={isDark}
+                setWhatUser={setWhatUser}
+                item={item.teammateRole}
+                state={item.teamReader ? 'Reader' : 'Member'}
+                name={item.memberName}
+              />
+            </motion.div>
+          );
+        } else {
+          return (
+            <motion.div
+              key={index}
+              style={{
+                display: "flex",
+                gap: "1rem",
+                flexDirection: "column",
+                height: "100%",
+              }}
+              variants={{
+                hidden: { opacity: 0, y: 20 },
+                visible: { opacity: 1, y: 0 },
+              }}
+            >
+              <FakeWhatPos isDark={isDark} />
+              <WhatPosCard
+                isDark={isDark}
+                setWhatUser={setWhatUser}
+                item={item.teammateRole}
+                state={item.teamReader ? 'Reader' : 'Member'}
+                name={item.memberName}
+              />
+            </motion.div>
+          );
+        }
+      })}
+    </motion.div>
+  );
+};
+
+export default ProjectTeam;
