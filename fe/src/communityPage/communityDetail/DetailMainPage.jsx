f261961e988b7c7ffdfa6f491eb3b903bbf0299a
전은희 <jeon78eun@naver.com>
2024-05-12 16:07:31 +0900

    Feat: 커뮤니티 상세 구현
    페이지 네이션 기능
    커뮤니티에서 팀 스페이스 이동 링크
    
    Related to: #24

--- fe/src/communityPage/communityDetail/DetailMainPage.jsx
+++ fe/src/communityPage/communityDetail/DetailMainPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65a957a0449e36d16d8c862cfeb895618eca887c1356b22916ad6da5736ab898
-size 3859
+oid sha256:125e4dc9505244f478df728df6b28b896ef279b36d5d4c90fabbece8b18f27b0
+size 4723
--- fe/src/communityPage/communityList/CommunityMainPage.jsx
+++ fe/src/communityPage/communityList/CommunityMainPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:422f7bbda5f60180da56e725008af496cdf52ae067420de0dd0982f98e425b61
-size 5476
+oid sha256:f04629f74321f4235fecb2100a76d16395e9dc8ba11ed535f76a6f9ca00e7e72
+size 5460
