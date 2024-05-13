4a8f4b27902d50d1acf0cea56fa546eec845c0f2
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-13 11:49:06 +0900

    Fix: img change
    img change

--- fe/src/oauthTestPage/Callback.jsx
+++ fe/src/oauthTestPage/Callback.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ee06a4765d0c34562842d2f8b25da7e6e4003f82d36a31606c230fc32f9d7ea
+oid sha256:b6ce005fb37abd12924b9246a047fc37cbf19cdd75c116ba0ac56b2a88a7c86d
 size 2645
