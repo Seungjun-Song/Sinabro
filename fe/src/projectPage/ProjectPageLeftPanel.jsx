a46180a522f704f2b57891c48ea8a7d21eb4d44f
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-13 13:36:22 +0900

    Feat: project teammate img
    Feat: project teammate img
    
    Related to #6

--- fe/src/projectPage/ProjectPageLeftPanel.jsx
+++ fe/src/projectPage/ProjectPageLeftPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51c8d24677cb009916a08f2c84bd9f3095331437aaa77c24e3e7b58e38279b19
-size 25636
+oid sha256:30fb56a71773a338219cc40fb6cb052fcb5b84d5f6e4866f2180781eaca11ccd
+size 26104
