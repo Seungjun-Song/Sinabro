8a68651f0d8ddd0ebb33b89fa8234dbc190dc87f
최재성 <we0620@naver.com>
2024-05-13 10:36:52 +0900

    Fix :Teamspace세로 스크롤 및 직업색상

--- fe/src/components/TeamSpaceDetailComponent/ProjectTeam.jsx
+++ fe/src/components/TeamSpaceDetailComponent/ProjectTeam.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05caead00d21c2eb3edfb65e3a115bc6722c04a3d7878aaf56c3624d6da2102c
-size 6007
+oid sha256:2e5e2ee0385ed02da77c8403812fd98b9ea49627f17d1a3b391d68d8d1f8d0b1
+size 6048
--- fe/src/myPage/MyPageSidePanel.jsx
+++ fe/src/myPage/MyPageSidePanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfc217377ca4d59fcbe54b8da5f6e24fcd948cf80fc570009748976c51885974
-size 4739
+oid sha256:a20b24297af6f7d3b63c6c6ef090caa03c311690436313043f846c10f961f590
+size 5126
