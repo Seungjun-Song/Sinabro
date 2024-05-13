f30ab0b39dbfbd3f88d2c2e2172cd26a7eef8e6d
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-13 10:42:17 +0900

    Feat: change my img
    change my img
    
    Related to #3

--- fe/src/myPage/MyPageSidePanel.jsx
+++ fe/src/myPage/MyPageSidePanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:235fb3edb9ecc5b35cb9a6cbba50cdd2ddaa2ef1cd14984d993582866a823832
-size 4689
+oid sha256:087c434373d72c8a02d515dfcb99ac9170e1adaca7a17cc643f69a989a305389
+size 5080
