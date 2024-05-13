c6d7bdb61cbbc987ddce0f5f64d3ecec51625b32
최재성 <we0620@naver.com>
2024-05-13 15:41:20 +0900

    Fix :소나큐브 Css수정

--- fe/src/projectPage/ProjectPage.jsx
+++ fe/src/projectPage/ProjectPage.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import ProjectPageLeftPanel from './ProjectPageLeftPanel';
-import ProjectPageRightPanel from './ProjectPageRightPanel';
-import { useDispatch, useSelector } from 'react-redux';
-import { Calender } from '../components/calender/Calender';
-import { getDatabase, onValue, ref } from 'firebase/database';
-import { setNewMessageState } from '../store/newMessageSlice';
-import { changeProjectChatState } from '../store/projectChatShow';
-import WebRTC from '../components/webrtc/WebRTC';
-import { clearProjectRoomId } from '../store/projectRoomIdSlice';
-import axios from 'axios';
-import getEnv from '../utils/getEnv';
-import ProjectLoadingPage from './ProjectLoadingPage';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import ProjectPageLeftPanel from "./ProjectPageLeftPanel";
+import ProjectPageRightPanel from "./ProjectPageRightPanel";
+import { useDispatch, useSelector } from "react-redux";
+import { Calender } from "../components/calender/Calender";
+import { getDatabase, onValue, ref } from "firebase/database";
+import { setNewMessageState } from "../store/newMessageSlice";
+import { changeProjectChatState } from "../store/projectChatShow";
+import WebRTC from "../components/webrtc/WebRTC";
+import { clearProjectRoomId } from "../store/projectRoomIdSlice";
+import axios from "axios";
+import getEnv from "../utils/getEnv";
+import ProjectLoadingPage from "./ProjectLoadingPage";
 
 const ProjectContainer = styled.div`
   position: relative; /* 부모 컨테이너를 기준으로 자식 요소의 위치를 설정하기 위해 */
@@ -34,14 +34,14 @@ const MessageContainer = styled.div`
   position: absolute;
   bottom: 3rem; /* 아래 여백 조절 */
   right: 3rem; /* 우측 여백 조절 */
-  border-left: 1rem solid #3EC8AF;
+  border-left: 1rem solid #3ec8af;
   border-radius: 0.5rem;
-  box-shadow: 2px 2px 1px 2px #B8B8B8;
+  box-shadow: 2px 2px 1px 2px #b8b8b8;
   background-color: rgba(255, 255, 255, 0.8);
   cursor: pointer;
   transition: transform 0.3s ease;
   &:hover {
-      transform: scale(1.05);
+    transform: scale(1.05);
   }
   display: flex;
   flex-direction: column;
@@ -53,32 +53,32 @@ const MessageHeader = styled.div`
   font-weight: bold;
   width: 100%;
   border-radius: 0 10px 0 0;
-`
+`;
 
 const MessageBody = styled.div`
   padding: 5px;
   width: 100%;
-`
+`;
 
 const ProjectPage = () => {
-  const isProjectCalenderShow = useSelector((state) => state.projectCalender)
-  const [lastMessage, setLastMessage] = useState(null)
-  const [isFirstMount, setIsFirstMount] = useState(true)
-  const [codeServerURL, setCodeServerURL] = useState(null)
-  const [loading, setLoading] = useState(true)
-  const [teammate, setTeammate] = useState([])
+  const isProjectCalenderShow = useSelector((state) => state.projectCalender);
+  const [lastMessage, setLastMessage] = useState(null);
+  const [isFirstMount, setIsFirstMount] = useState(true);
+  const [codeServerURL, setCodeServerURL] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [teammate, setTeammate] = useState([]);
 
   const dispatch = useDispatch();
 
-  const newMessageInfo = useSelector(state => state.newMessage)
-  const myCurrentProject = useSelector(state => state.myCurrentProject.value)
-  console.log(myCurrentProject)
+  const newMessageInfo = useSelector((state) => state.newMessage);
+  const myCurrentProject = useSelector((state) => state.myCurrentProject.value);
+  console.log(myCurrentProject);
 
-  const back_url = getEnv('BACK_URL')
+  const back_url = getEnv("BACK_URL");
 
   useEffect(() => {
     const db = getDatabase();
-    const chatRef = ref(db, 'chats');
+    const chatRef = ref(db, "chats");
 
     onValue(chatRef, (snapshot) => {
       const data = snapshot.val();
@@ -94,97 +94,109 @@ const ProjectPage = () => {
       }
     });
     return () => {
-      dispatch(clearProjectRoomId()) // 프로젝트 페이지를 떠나면 프로젝트 Id를 삭제
-    }
+      dispatch(clearProjectRoomId()); // 프로젝트 페이지를 떠나면 프로젝트 Id를 삭제
+    };
   }, []);
 
   useEffect(() => {
     const getCodeServerURL = async () => {
       try {
-        const res = await axios.post(`${back_url}/teams/projects/enter`, {
-          repoUrl: myCurrentProject.projectRepo
-        },{withCredentials: true})
-        console.log(res.data)
-        setCodeServerURL(res.data.result.url)
-        setLoading(false)
+        const res = await axios.post(
+          `${back_url}/teams/projects/enter`,
+          {
+            repoUrl: myCurrentProject.projectRepo,
+          },
+          { withCredentials: true }
+        );
+        console.log(res.data);
+        setCodeServerURL(res.data.result.url);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
       }
-      catch (err) {
-        console.error(err)
-      }
-    }
-    getCodeServerURL()
+    };
+    getCodeServerURL();
 
     const leaveCodeServer = async () => {
       try {
-        const res = await axios.post(`${back_url}/teams/projects/exit`,{withCredentials: true})
-        console.log(res.data)
+        const res = await axios.post(`${back_url}/teams/projects/exit`, {
+          withCredentials: true,
+        });
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
       }
-      catch (err) {
-        console.log(err)
-      }
-    }
+    };
 
     return () => {
-      setCodeServerURL(null) // 프로젝트를 떠나면 주소 초기화
-      setLoading(true)
-      leaveCodeServer()
-    }
-  }, [])
+      setCodeServerURL(null); // 프로젝트를 떠나면 주소 초기화
+      setLoading(true);
+      leaveCodeServer();
+    };
+  }, []);
 
   useEffect(() => {
     const getTeammateInfo = async () => {
       try {
-        const res = await axios.get(`${back_url}/teams?projectId=${myCurrentProject.projectId}`)
-        console.log('팀원 정보', res.data.result.teammateInfoList)
-        setTeammate(res.data.result.teammateInfoList)
-        console.log('팀메이트', teammate)
+        const res = await axios.get(
+          `${back_url}/teams?projectId=${myCurrentProject.projectId}`
+        );
+        console.log("팀원 정보", res.data.result.teammateInfoList);
+        setTeammate(res.data.result.teammateInfoList);
+        console.log("팀메이트", teammate);
+      } catch (err) {
+        console.error(err);
       }
-      catch (err) {
-        console.error(err)
-      }
-    }
-    getTeammateInfo()
-  }, [])
+    };
+    getTeammateInfo();
+  }, []);
 
   const newChatState = () => {
-    if (!isFirstMount && newMessageInfo.isNotificationOn && !newMessageInfo.projectRightPanelState && newMessageInfo.newMessageState) { //처음 마운트가 아니고 알림이 켜져있으면
-      return true
+    if (
+      !isFirstMount &&
+      newMessageInfo.isNotificationOn &&
+      !newMessageInfo.projectRightPanelState &&
+      newMessageInfo.newMessageState
+    ) {
+      //처음 마운트가 아니고 알림이 켜져있으면
+      return true;
+    } else {
+      return false;
     }
-    else {
-      return false
-    }
-  }
+  };
 
   return (
     <>
-      {loading ?
+      {loading ? (
         <ProjectLoadingPage />
-        :
+      ) : (
         <ProjectContainer>
           <WebRTC />
           <ProjectMainContainer>
             <ProjectPageLeftPanel teammate={teammate} />
             {isProjectCalenderShow.value === true ? (
-              <div style={{ height: '100%', width: '100%' }}>
+              <div style={{ height: "100%", width: "100%" }}>
                 <Calender />
               </div>
             ) : (
               <iframe
                 title="code-server"
                 src={codeServerURL}
-                style={{ width: '100%', height: '100%', border: 'none' }}
+                style={{ width: "100%", height: "100%", border: "none" }}
               ></iframe>
             )}
             <ProjectPageRightPanel />
             {newChatState() && (
-              <MessageContainer onClick={() => dispatch(changeProjectChatState(true))}>
+              <MessageContainer
+                onClick={() => dispatch(changeProjectChatState(true))}
+              >
                 <MessageHeader>{lastMessage.displayName}</MessageHeader>
                 <MessageBody>{lastMessage.message}</MessageBody>
               </MessageContainer>
             )}
           </ProjectMainContainer>
         </ProjectContainer>
-      }
+      )}
     </>
   );
 };
