6e7a8335dface55b960bed81ee736e752dc7126b
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-14 14:03:38 +0900

    Fix: chat error fix
    Fix: chat error fix
    
    Related to #6

--- fe/src/projectPage/ProjectPage.jsx
+++ fe/src/projectPage/ProjectPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ade81daffd506875e6e9870d352b32f068292faa0f64e260e663267941dbcc78
-size 6881
+oid sha256:d31176f4c8d1e7df6627e482b4399e03a54993e166979a045a627f84cbf4a5e1
+size 6880
