88e95f7e23402197e51e7b902b8ef2be1672377f
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-14 17:37:50 +0900

    Feat: iframe change
    Feat: iframe change

--- fe/src/projectPage/ProjectPage.jsx
+++ fe/src/projectPage/ProjectPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ae186fd34fe62427079336a005200598eebe65bc0d33db019821d044a8198aa
-size 8645
+oid sha256:c4e6385790ffcf33843b7cf4a133366b52ca762b26c68a564ca8237f260e4de6
+size 8635
