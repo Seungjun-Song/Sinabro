e73c1dd25f7c0b2af419ea68b1935416e1f80cb5
전은희 <jeon78eun@naver.com>
2024-05-14 12:10:22 +0900

    Fix: 커뮤니티 리스트 오류 수정
    커뮤니티 리스트 출력 시 오류 수정
    
    Related to: #24

--- fe/src/communityPage/communityList/Post.jsx
+++ fe/src/communityPage/communityList/Post.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0eb0c9047452b0cc069ea365f191d337bc203677b7343d84f316e59c13f69772
-size 6242
+oid sha256:f7a4971f2fcce2aefeee24b7776d17d7d179cb06a6f9af8d7b8b0211f7b9a505
+size 6246
