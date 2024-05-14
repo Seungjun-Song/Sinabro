797b73bfa8890c342eb43d83ccd0e63160627962
송승준 <ssj0187@naver.com>
2024-05-14 16:14:58 +0900

    Fix: 프로젝트 피드백 초대 에러 해결

--- be/src/main/java/com/HP50/be/domain/community/repository/BoardCustomRepository.java
+++ be/src/main/java/com/HP50/be/domain/community/repository/BoardCustomRepository.java
@@ -1,3 +1,91 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:66dd4c4ff3987fa52fc6ea3db81438f287ce0de75e6f219d14f0e90af6bdc830
-size 3295
+package com.HP50.be.domain.community.repository;
+
+
+
+import com.HP50.be.domain.code.entity.SubCategory;
+import com.HP50.be.domain.code.repository.CategoryRepository;
+import com.HP50.be.domain.code.service.SubCategoryService;
+import com.HP50.be.domain.community.dto.BoardFilterRequestDto;
+import com.HP50.be.domain.community.dto.SliceTotalCountDto;
+import com.HP50.be.domain.community.entity.Board;
+import com.HP50.be.domain.member.entity.Member;
+import com.HP50.be.global.jwt.JwtUtil;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+import java.util.Random;
+
+import static com.HP50.be.domain.community.entity.QBoard.board;
+
+@Repository
+@RequiredArgsConstructor
+@Transactional
+public class BoardCustomRepository {
+    private final JPAQueryFactory queryFactory;
+    private final SubCategoryService subCategoryService;
+
+    public SliceTotalCountDto<Board> findByConditions(Integer boards,
+                                         Integer calender,
+                                         Integer job,
+                                         String keyword,
+                                         Pageable pageable) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+
+        if (boards != null && boards != 0) {
+            builder.and(board.subCategory.eq(subCategoryService.findById(boards)));
+        }
+
+        if (calender != null && calender != 0) {
+            if (calender == 502) builder.and(board.communityProgress.eq(true));
+            else if (calender == 503) builder.and(board.communityProgress.eq(false));
+        }
+
+        if (job != null && job != 0) {
+            if (job == 100) builder.and(board.requiredPeopleFrontEnd.gt(0));
+            if (job == 200) builder.and(board.requiredPeopleBackEnd.gt(0));
+        }
+
+        if (keyword != null && !keyword.isEmpty()) {
+            builder.and(board.boardTitle.like("%" + keyword + "%"));
+            builder.and(board.boardContent.like("%" + keyword + "%"));
+        }
+
+        List<Board> results = queryFactory
+                .selectFrom(board)
+                .where(builder)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize()+1)
+                .orderBy(board.createdDttm.desc())
+                .fetch();
+
+        int totalCount = queryFactory
+                .selectFrom(board)
+                .where(builder)
+                .fetch()
+                .size();
+
+        boolean hasNext = results.size() > pageable.getPageSize();
+
+        List<Board> boardList = hasNext ? results.subList(0, pageable.getPageSize()) : results;
+
+        return new SliceTotalCountDto<>(new SliceImpl<>(boardList, pageable, hasNext), totalCount);
+    }
+
+    public List<Board> getRandomBoardRequired(){
+        return queryFactory.selectFrom(board)
+                .where(board.subCategory.subCategoryId.eq(401))
+                .where(board.project.isNotNull())
+                .limit(10)
+                .fetch();
+    }
+
+}
--- be/src/main/java/com/HP50/be/domain/community/repository/CommentCustomRepository.java
+++ be/src/main/java/com/HP50/be/domain/community/repository/CommentCustomRepository.java
@@ -1,3 +1,44 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:86891de7fb831316388533e4f869b2e27bb2a03b251eb36980c0b42682ca4eb5
-size 1689
+package com.HP50.be.domain.community.repository;
+
+import com.HP50.be.domain.community.entity.Board;
+import com.HP50.be.domain.community.entity.Comment;
+import com.HP50.be.global.common.StatusCode;
+import com.HP50.be.global.exception.BaseException;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.HP50.be.domain.community.entity.QBoard.board;
+import static com.HP50.be.domain.community.entity.QComment.comment;
+
+@Repository
+@RequiredArgsConstructor
+@Transactional
+public class CommentCustomRepository {
+    private final JPAQueryFactory queryFactory;
+    private final BoardRepository boardRepository;
+
+    public Slice<Comment> findCommentByBoard(Integer boardId, Pageable pageable) {
+        Board board = boardRepository.findById(boardId).orElseThrow(() -> new BaseException(StatusCode.NOT_EXIST_MEMBER));
+
+        List<Comment> results = queryFactory
+                .selectFrom(comment)
+                .where(comment.board.eq(board))
+                .orderBy(comment.commentId.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        boolean hasNext = results.size() > pageable.getPageSize();
+
+        List<Comment> comments = hasNext ? results.subList(0, pageable.getPageSize()) : results;
+
+        return new SliceImpl<>(comments, pageable, hasNext);
+    }
+}
