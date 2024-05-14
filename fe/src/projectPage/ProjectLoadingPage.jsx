cf623e83e0344b6f00f8b07469aa80e99a24ab8e
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-14 17:52:42 +0900

    Fix: project page css
    Fix: project page css
    
    Related to #6

--- fe/src/projectPage/ProjectLoadingPage.jsx
+++ fe/src/projectPage/ProjectLoadingPage.jsx
@@ -1,21 +1,3 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-
-const ProjectLoadingPage = () => {
-    const myCurrentProject = useSelector(state => state.myCurrentProject.value);
-    const isDark = useSelector(state => state.isDark.isDark)
-
-    return (
-        <div className="d-flex flex-column justify-content-center align-items-center vh-100" style={{backgroundColor: isDark ? 'grey' :'whitesmoke'}}>
-            <div className="text-center mb-3">
-                <h2 style={{color: isDark ? 'whitesmoke' : 'black'}}>Loading...</h2>
-                <p style={{color: isDark ? 'whitesmoke' : 'black'}}>Please wait while <span style={{fontWeight: 'bold', color: '#564CAD'}}>{myCurrentProject.projectName}</span> is loading.</p>
-            </div>
-            <div className="spinner-border text-primary" role="status">
-                <span className="visually-hidden">Loading...</span>
-            </div>
-        </div>
-    );
-}
-
-export default ProjectLoadingPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:d86cdd014d60641178e07610d2124e4f32ed9442bbb84faa3df7de92e2b1addb
+size 976
