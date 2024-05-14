96a5452a742a4f40458b7be9fc0504962b70a801
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-14 14:30:14 +0900

    Fix: code server dark mode
    Fix: code server dark mode
    
    Related to #6

--- fe/src/projectPage/ProjectPage.jsx
+++ fe/src/projectPage/ProjectPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b738f11c075fcdfff6acf0b97915645a8146ac08d24ace734a5ced202f13150e
-size 7032
+oid sha256:7aa5f18381eeb13a41bcdd0ca543efcf22c509f5e9a78432af1f932d9ce82970
+size 7184
