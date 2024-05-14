c0d115dbe375346128b329719cfff8c64c570681
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-14 14:18:37 +0900

    Feat: code server dark mode
    code server dark mode
    
    Related to #6

--- fe/src/communityPage/communityList/styles/CommunityMemberPageDefault.js
+++ fe/src/communityPage/communityList/styles/CommunityMemberPageDefault.js
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:88899a303cefc15f4287665278b2048238a3351b6c07ccf1353281fbbfbbb6d3
-size 149
+const MemberPage = {
+    background: 'white',
+}
+
+
+const CommunityMemberPageDefault = {
+    MemberPage,
+}
+
+export default CommunityMemberPageDefault;
