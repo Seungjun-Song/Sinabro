cd763ffcd4e469917aca43fc6887631fbfb07056
박종국 <whdrnrdl789@naver.com>
2024-05-14 16:15:17 +0900

    Fix: All Calender to Milestone
    기존의 캘린더 API 에 milestone 추가완료
    
    Related to #33

--- be/src/main/java/com/HP50/be/domain/calender/dto/MyCalenderDto.java
+++ be/src/main/java/com/HP50/be/domain/calender/dto/MyCalenderDto.java
@@ -1,28 +1,3 @@
-package com.HP50.be.domain.calender.dto;
-
-import lombok.*;
-
-import java.time.LocalDate;
-
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class MyCalenderDto {
-    private Integer calenderId;
-    private Integer subCategoryId;
-    private LocalDate calenderStartDt;
-    private LocalDate calenderEndDt;
-    private String calenderName;
-    private Integer milestoneId;
-    private String milestoneName;
-
-    public MyCalenderDto(Integer calenderId, Integer subCategoryId, LocalDate calenderStartDt, LocalDate calenderEndDt, String calenderName) {
-        this.calenderId = calenderId;
-        this.subCategoryId = subCategoryId;
-        this.calenderStartDt = calenderStartDt;
-        this.calenderEndDt = calenderEndDt;
-        this.calenderName = calenderName;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:98a37ab65648fb0f47048b1cd6068da6bb2501fc94e0ed7de5d1f8d1c9af89a2
+size 427
