fe50c2f009d9002837352e3a2946bd1f4d19dda1
김윤민 <qkslzl101216@naver.com>
2024-05-14 17:10:59 +0900

    Feat: mkdir
    mkdir

--- be/src/main/java/com/HP50/be/domain/project/service/SonarQubeServiceImpl.java
+++ be/src/main/java/com/HP50/be/domain/project/service/SonarQubeServiceImpl.java
@@ -222,7 +222,7 @@ public class SonarQubeServiceImpl implements SonarQubeService{
            - exec command
          */
         // 폴더에 있는 repo 다 삭제 후 mkdir
-        String deleteRepo = "sudo rm -rf /home/ubuntu/sonarQube/scanner/"+projectId+" && "+"sudo mkdir /home/ubuntu/sonarQube/scanner/"+projectId;
+        String deleteRepo = "sudo rm -rf /home/ubuntu/sonarQube/scanner/"+projectId+" && "+"sudo mkdir -p /home/ubuntu/sonarQube/scanner/"+projectId;
 
         // 폴더 이동 command & git clone
         String toFolderAndClone = "cd /home/ubuntu/sonarQube/scanner/"+projectId+" && " + "sudo git clone "+repoUrl;
