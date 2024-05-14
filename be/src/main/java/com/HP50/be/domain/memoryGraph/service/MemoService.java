7ab0f9c829855038d518910bd0979ad7f84f43f9
박종국 <whdrnrdl789@naver.com>
2024-05-14 16:29:51 +0900

    Fix: MemoryGraph Update API FIX

--- be/src/main/java/com/HP50/be/domain/memoryGraph/service/MemoService.java
+++ be/src/main/java/com/HP50/be/domain/memoryGraph/service/MemoService.java
@@ -1,14 +1,13 @@
 package com.HP50.be.domain.memoryGraph.service;
 
 import com.HP50.be.domain.memoryGraph.dto.MemberForGraphDto;
-import com.HP50.be.domain.memoryGraph.dto.MemoDto;
-
-import java.util.List;
+import com.HP50.be.domain.memoryGraph.dto.MemoRequestDto;
+import com.HP50.be.domain.memoryGraph.dto.MemoResponseDto;
 
 public interface MemoService {
     MemberForGraphDto findMemoByMemberId(Integer memberId);
-    void saveMemo(String token, MemoDto memoDto);
+    void saveMemo(String token, MemoResponseDto memoResponseDto);
     void setMemoToMemo(String token, String firstMemoId, String secondMemoId);
     void deleteMemo(String memoId);
-    void updateMemo(MemoDto memoDto);
+    void updateMemo(MemoRequestDto memoRequestDto);
 }
