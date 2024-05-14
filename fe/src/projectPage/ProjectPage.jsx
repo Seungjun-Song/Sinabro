4c357d0796af72433ec3c825b5ac51c17d599e51
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-14 17:44:29 +0900

    Fix: fix bug in project page
    Fix: fix bug in project page
    
    Related to #6

--- fe/src/projectPage/ProjectPage.jsx
+++ fe/src/projectPage/ProjectPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3aaa18cb921337d7646f223101c1ebbc523fd1cbc050e6f56165e7254178afc
-size 8649
+oid sha256:9771a5ba81231b29dd2194e8c6ce4e69206da3d2e27182869b149f62fa2a78be
+size 8638
