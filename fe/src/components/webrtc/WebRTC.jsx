feec36bfc03140a1688f69f9d8e188bef6c791b8
최재성 <we0620@naver.com>
2024-05-16 16:36:09 +0900

    Feat : 프로젝트 사용법알려주는거 초안

--- fe/src/components/webrtc/WebRTC.jsx
+++ fe/src/components/webrtc/WebRTC.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bedf077abff2c09005da0c5ddab6cbb4e45fe245980cadc4c3c7705dcf0bdc1a
-size 10533
+oid sha256:ea9b5f0662edf7b026e1346113463ad6b85be866fddbceab7ee3b7284c007d56
+size 13245
