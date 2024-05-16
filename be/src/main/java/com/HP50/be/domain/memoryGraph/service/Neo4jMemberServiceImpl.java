358cc6f190053a8e2c15e68543765dc1c3581cb7
박종국 <whdrnrdl789@naver.com>
2024-05-16 09:32:26 +0900

    Fix: Implement memo addition in profile API

--- be/src/main/java/com/HP50/be/domain/memoryGraph/service/Neo4jMemberServiceImpl.java
+++ be/src/main/java/com/HP50/be/domain/memoryGraph/service/Neo4jMemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f966bb5dbbaa272710e89c588423e8cb3e8370410be6b0bd732ee82793553b1
-size 3259
+oid sha256:8d9bb58c66f69fa98158bd4829d04be3c8eaf65a7bb41bebcacb199f2b6c94ed
+size 4015
