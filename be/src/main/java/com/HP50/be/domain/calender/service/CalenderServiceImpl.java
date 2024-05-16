216b68192c9e41bbdc3e49af28f168a9ff7554f3
박종국 <whdrnrdl789@naver.com>
2024-05-16 10:53:24 +0900

    Fix: Integrate Milestone property in Calendar DTO and adjust service logic

--- be/src/main/java/com/HP50/be/domain/calender/service/CalenderServiceImpl.java
+++ be/src/main/java/com/HP50/be/domain/calender/service/CalenderServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb9240088e1ee5d6ea26d9c02f28419d9699d02f6cd2862a6a04a1e5ba6e4d5c
-size 6299
+oid sha256:aa8154524ccfa1c095ed0760516e8e1a2042836adb26b65d4e51bc9b33bb7753
+size 6683
