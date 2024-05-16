9bd5d4d4b54b2ee9beae2e3e16cd9ed9394bc428
전은희 <jeon78eun@naver.com>
2024-05-16 17:35:38 +0900

    Feat: 수정사항 반영
    Related to: #34

--- fe/src/components/teamspaceComponents/ProjectCreateBtn.jsx
+++ fe/src/components/teamspaceComponents/ProjectCreateBtn.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:319b90b9c88f4b43890b60edefa43847622701f5be1d481d4b5b1b67f127c221
-size 2424
+oid sha256:a8a76be63fb71b243ea8004eb8dc56ad8720d18070015eb0eeb221b620183ac2
+size 2403
--- fe/src/teamspacePage/TeamSpacePage.jsx
+++ fe/src/teamspacePage/TeamSpacePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:442d8bdb13c59b546dc33acef66ec088a839bd92feb222687e44d0f33ea370cf
-size 3696
+oid sha256:72a8cfda91c18d222b2cb4051de25e7050da0376b93a989cd67bd427c65ea2f5
+size 3715
