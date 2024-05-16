1c2f47faa60ac6c70d03c34a60289370c2021751
전은희 <jeon78eun@naver.com>
2024-05-16 12:04:13 +0900

    Feat: 커뮤니티 개인 채팅
    커뮤니티 개인 채팅 완성
    
    Related to: #24

--- fe/src/communityPage/communityDetail/WriterLinkBox.jsx
+++ fe/src/communityPage/communityDetail/WriterLinkBox.jsx
@@ -1,3 +1,79 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d5d2ded77d90d2824aba84f2ac65c23f4e0357597d9c4f015b1d06970a2d8a2a
-size 1666
+import styled from 'styled-components'
+import { motion } from "framer-motion"
+
+import MoveToPjt from '/image/community/moveToPjt.png'
+import { useNavigate } from 'react-router-dom'
+
+const WriterBox = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    gap: 3rem;
+
+    padding: 0.5rem 0.6rem 0.5rem 0.5rem;
+    background: linear-gradient(to right, rgba(216, 245, 242, 1), rgba(242, 244, 179, 1));
+
+    border: 1px solid rgba(216, 245, 242, 1);
+    border-radius: 10px;
+
+    color: black;
+
+
+`
+
+const WriterInfo = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: start;
+
+    height: 2rem;
+
+    gap: 1rem;
+`
+
+const WriterProfile = styled.img`
+    height: 3rem;
+
+    border: 0px solid black;
+    border-radius: 50px;
+
+    height: 90%;
+
+`
+
+const WriterName = styled.div`
+    font-family: Pretendard Medium;
+`
+
+const WriterLink = styled.img`
+`
+
+const WriterLinkBox = ({detailData}) => {
+
+    const navigate = useNavigate();
+    //console.log("writerBox", detailData)
+    return(
+        <WriterBox
+            whileHover={{ cursor: "pointer", y: -3}}
+            onClick={() => navigate(`/writerPage`, {state: {memberId: detailData.memberId}})}
+        >
+            <WriterInfo>
+                <WriterProfile
+                    src={detailData.writerprofile}>
+                </WriterProfile>
+                <WriterName>
+                    @{detailData.writername}
+                </WriterName>
+            </WriterInfo>
+
+        
+            <WriterLink
+                src={MoveToPjt}
+            >
+            </WriterLink>
+        </WriterBox>
+    )
+}
+
+export default WriterLinkBox;
--- fe/src/communityPage/createPost/CreateJobsBox.jsx
+++ fe/src/communityPage/createPost/CreateJobsBox.jsx
@@ -1,3 +1,139 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3dd11bc4d53c5573ec94a8e870a2a249a398571eeba642f8cd07364cf12abfa8
-size 3761
+import styled, { css } from 'styled-components'
+import { faDesktop, faCog, faLeaf } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+
+const JobBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: start;
+
+    gap: 0.5rem;
+`
+
+const Job = styled.div`
+    border: 3px solid ${props => props.borderColor};
+    color: ${props => props.borderColor};
+
+    padding: 0.5rem 0.5rem;
+    gap: 10px;
+    border-radius: 5px;
+    max-height: 5rem;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    box-shadow: 3px 4px 3px rgba(217, 217, 217, 1);
+    font-size: 0.8rem;
+
+    cursor: pointer;
+
+    ${props => props.selected && css`
+        background: ${props => props.borderColor};
+        color: white;
+    `}
+
+`
+
+const InputBox = styled.input`
+    width: 0.5rem;
+    border: none;
+    padding: 0;
+
+`
+
+const CreateJobsBox = ({kind, jobInfo, setJobInfo}) => {
+
+    const selectJob = (job, index) =>{
+       
+        setJobInfo(jobInfo.map((v, i)=>{
+            if( index !== i){
+                return v;
+            }
+            else{
+                return {...v, selected: 1-job.selected}
+            }
+        }))
+    }
+
+    const changeTargetVal = (data, index) => {
+
+        setJobInfo(jobInfo.map((v, i) => {
+            if(index !== i ){
+                return v;
+            }
+            else{
+                if(data.nativeEvent.data == null || data.nativeEvent.data == "")
+                    return {...v, target: ""}
+                else
+                    return{...v, target: parseInt(data.nativeEvent.data)};
+            }
+        }))
+    
+
+    }
+
+    const changeTotalVal = (data, index) => {
+        setJobInfo(jobInfo.map((v, i)=>{
+            if(index !== i){
+                return v;
+            }
+            else{
+                if(data.nativeEvent.data == null || data.nativeEvent.data == ""){
+                    return {...v, total: ""}
+                }
+                else
+                    return{...v, total: parseInt(data.nativeEvent.data)}
+            }
+        }))
+
+    }
+    return(
+        <JobBox>
+            {kind.name == "member" && 
+            <>
+                {jobInfo.map((job, index) => (
+                <Job
+                    borderColor={job.borderColor}
+                >
+                    {job.icon && <FontAwesomeIcon icon={job.icon} style={{ fontSize: '15px' }} />}
+                    <div>
+                    {job.name}
+                    </div>
+                    <InputBox
+                        value={job.total}
+                        onChange={(data) => changeTotalVal(data, index)}
+                    >
+                    </InputBox> / 
+                    <InputBox
+                        value={job.target}
+                        onChange={(data) => changeTargetVal(data, index)}
+                    >
+                    </InputBox>
+                </Job>
+            ))}               
+            6명 이하 인원만 가능해요!
+            </>}
+            
+            {kind.name == "feadback" &&
+            <>
+                {jobInfo.map((job, index) => (
+                <Job
+                    borderColor={job.borderColor}
+                    selected={job.selected === 1}
+                    onClick={() => selectJob(job, index)}
+                >
+                    {job.icon && <FontAwesomeIcon icon={job.icon} style={{ fontSize: '10px' }} />}
+                    <div>
+                    {job.name}
+                    {console.log(job.selected)}
+                    </div>
+                </Job>
+                ))}   
+            </>
+            }
+        
+        </JobBox>
+    )
+}
+
+export default CreateJobsBox;
