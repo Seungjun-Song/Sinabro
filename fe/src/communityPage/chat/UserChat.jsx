00d8848ac8bf535cb3bf918cbc5a653e3570590b
전은희 <jeon78eun@naver.com>
2024-05-16 09:54:27 +0900

    Fix: 채팅 비활성화
    Related to: #24

--- fe/src/communityPage/chat/UserChat.jsx
+++ fe/src/communityPage/chat/UserChat.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:721bf49c42b8e59e000db08e7675d7d310c85af2be60ba0949e6595a6e33db14
-size 9539
+oid sha256:a727bf01c1bf96dbb16df9631d42035d17bfa2db15c6e2bd6a1313e8e6ab4fa6
+size 9623
