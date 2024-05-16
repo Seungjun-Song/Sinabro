852d45bf63eb123f94351a7668db485307d1a528
최재성 <we0620@naver.com>
2024-05-16 10:27:13 +0900

    Feat :메모리그래프 수정

---
+++ fe/src/projectPage/ProjectInfo.jsx
@@ -0,0 +1,20 @@
+const ProjectInfo = () => {
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          backgroundColor: "rgba(0,0,0,0.3)",
+        }}
+      >
+        {/* <div style={{ backgroundColor: "rgba(255,255,255,0.5)" }}>adsf</div> */}
+      </div>
+    </>
+  );
+};
+
+export default ProjectInfo;
--- fe/src/store/store.jsx
+++ fe/src/store/store.jsx
@@ -1,3 +1,53 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8b6aaad6e5a8931712b4595e688e85b86751d49d9bfeb5ab3b9246aaf09c8068
-size 1818
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import userSlice from "./userSlice";
+import projectCalenderSlice from "./projectCalenderSlice";
+import toDoListSlice from "./toDoListSlice";
+import addScheduleModalHandleSlice from "./addScheduleModalHandleSlice";
+import newMessageSlice from "./newMessageSlice";
+import projectChatShowSlice from "./projectChatShow";
+import isDarkSlice from "./isDarkSlice";
+import projectCreateSlice from "./projectCreateSlice";
+import userSearchSlice from "./userSearchSlice";
+import inviteUserSlice from "./inviteUserSlice";
+import invitedUserListSlice from "./invitedUserListSlice";
+import projectRoomIdSlice from "./projectRoomIdSlice";
+import myProjectListSlice from "./myProjectListSlice";
+import myCurrentProjectSlice from "./myCurrentProjectSlice";
+
+const reducers = combineReducers({
+  user: userSlice,
+  projectCalender: projectCalenderSlice,
+  toDoList: toDoListSlice,
+  addScheduleModalHandle: addScheduleModalHandleSlice,
+  newMessage: newMessageSlice,
+  projectChatShow: projectChatShowSlice,
+  isDark : isDarkSlice,
+  projectCreate: projectCreateSlice,
+  userSearch: userSearchSlice,
+  inviteUser: inviteUserSlice,
+  invitedUserList: invitedUserListSlice,
+  projectRoomId: projectRoomIdSlice,
+  myProjectList: myProjectListSlice,
+  myCurrentProject: myCurrentProjectSlice,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
