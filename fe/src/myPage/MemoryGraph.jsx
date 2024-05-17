38baf9e6d997b7fd851c4ae168609a723c040ed5
최재성 <we0620@naver.com>
2024-05-17 12:17:11 +0900

    Fix :수정중

--- fe/src/myPage/MemoryGraph.jsx
+++ fe/src/myPage/MemoryGraph.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:595915653c136f756a94ad470a15fddbd55efd3623a24e2f57d749aeefbeb478
-size 20894
+oid sha256:d160d1d75158264572c32d3f815411c7342f2bee92cf37a8efd3cc75bef99132
+size 20874
