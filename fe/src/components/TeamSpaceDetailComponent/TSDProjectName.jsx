ce3f5aaa9209825391b24b193e5712882ae3c849
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-17 15:38:54 +0900

    Fix: survey page error
    Fix: survey page error
    
    Related to #1

--- fe/src/components/TeamSpaceDetailComponent/TSDProjectName.jsx
+++ fe/src/components/TeamSpaceDetailComponent/TSDProjectName.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c05cc83b5ce03dcf12606ca6a8ee8bd53b1426e25a49585857cf574f4ffe3a42
-size 5866
+oid sha256:bb62b31b92f31fcdefffffb70bf02252f35f75ec3d6886c39680e2f064a1e83d
+size 5880
