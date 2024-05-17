ac71be3f8960aa674b6f43530d79dd38473337d2
박종국 <whdrnrdl789@naver.com>
2024-05-17 17:07:13 +0900

    Feat: Board People Plus Minus Button & Disabled Input Field Editing

--- fe/src/communityPage/createPost/CreateJobsBox.jsx
+++ fe/src/communityPage/createPost/CreateJobsBox.jsx
@@ -1,20 +1,17 @@
-import styled, { css } from 'styled-components'
-import { faDesktop, faCog, faLeaf } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
 
 const JobBox = styled.div`
     display: flex;
     align-items: center;
     justify-content: start;
-
     gap: 0.5rem;
-`
+`;
 
 const Job = styled.div`
     border: 3px solid ${props => props.borderColor};
     color: ${props => props.borderColor};
-
     padding: 0.5rem 0.5rem;
     gap: 10px;
     border-radius: 5px;
@@ -24,94 +21,138 @@ const Job = styled.div`
     align-items: center;
     box-shadow: 3px 4px 3px rgba(217, 217, 217, 1);
     font-size: 0.8rem;
-
     cursor: pointer;
 
     ${props => props.selected && css`
         background: ${props => props.borderColor};
         color: white;
     `}
-
-`
+`;
 
 const InputBox = styled.input`
     width: 0.5rem;
     border: none;
     padding: 0;
+`;
 
-`
+const Button = styled.button`
+    height: 0.5rem;
+    width: 0.5rem;
+    border: none;
+    background: none;
+`;
+
+const VerticalContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
 
 const CreateJobsBox = ({kind, jobInfo, setJobInfo}) => {
-
-    const selectJob = (job, index) =>{
-       
-        setJobInfo(jobInfo.map((v, i)=>{
-            if( index !== i){
+    const selectJob = (job, index) => {
+        setJobInfo(jobInfo.map((v, i) => {
+            if(index !== i){
                 return v;
-            }
-            else{
+            } else {
                 return {...v, selected: 1-job.selected}
             }
         }))
     }
 
-    const changeTargetVal = (data, index) => {
-
-        setJobInfo(jobInfo.map((v, i) => {
-            if(index !== i ){
-                return v;
-            }
-            else{
-                if(data.nativeEvent.data == null || data.nativeEvent.data == "")
-                    return {...v, target: ""}
-                else
-                    return{...v, target: parseInt(data.nativeEvent.data)};
-            }
-        }))
-    
-
-    }
-
     const changeTotalVal = (data, index) => {
-        setJobInfo(jobInfo.map((v, i)=>{
+        setJobInfo(jobInfo.map((v, i) => {
             if(index !== i){
                 return v;
-            }
-            else{
+            } else {
                 if(data.nativeEvent.data == null || data.nativeEvent.data == ""){
                     return {...v, total: ""}
-                }
-                else
+                } else {
                     return{...v, total: parseInt(data.nativeEvent.data)}
+                }
             }
         }))
-
     }
-    return(
+
+    const changeTargetlVal = (data, index) => {
+        setJobInfo(jobInfo.map((v, i) => {
+            if(index !== i){
+                return v;
+            } else {
+                if(data.nativeEvent.data == null || data.nativeEvent.data == ""){
+                    return {...v, target: ""}
+                } else {
+                    return{...v, target: parseInt(data.nativeEvent.data)}
+                }
+            }
+        }))
+    }
+
+    const incrementTotalVal = (index) => {
+        setJobInfo(jobInfo.map((v, i) => {
+            if(index !== i){
+                return v;
+            } else {
+                return {...v, total: v.total + 1}
+            }
+        }))
+    }
+
+    const decrementTotalVal = (index) => {
+        setJobInfo(jobInfo.map((v, i) => {
+            if(index !== i){
+                return v;
+            } else {
+                return {...v, total: v.total - 1}
+            }
+        }))
+    }
+
+    const incrementTargetlVal = (index) => {
+        setJobInfo(jobInfo.map((v, i) => {
+            if(index !== i){
+                return v;
+            } else {
+                return {...v, target: v.total + 1}
+            }
+        }))
+    }
+
+    const decrementTargetlVal = (index) => {
+        setJobInfo(jobInfo.map((v, i) => {
+            if(index !== i){
+                return v;
+            } else {
+                return {...v, target: v.target - 1}
+            }
+        }))
+    }
+
+    return (
         <JobBox>
-            {kind.name == "member" && 
+            {kind.name === "member" && 
             <>
                 {jobInfo.map((job, index) => (
-                <Job
-                    borderColor={job.borderColor}
-                >
-                    {job.icon && <FontAwesomeIcon icon={job.icon} style={{ fontSize: '15px' }} />}
-                    <div>
-                    {job.name}
-                    </div>
-                    <InputBox
-                        value={job.total}
-                        onChange={(data) => changeTotalVal(data, index)}
-                    >
-                    </InputBox> / 
-                    <InputBox
-                        value={job.target}
-                        onChange={(data) => changeTargetVal(data, index)}
-                    >
-                    </InputBox>
-                </Job>
-            ))}               
-            3명 이하 인원만 가능해요!
+                    <Job borderColor={job.borderColor}>
+                        {job.icon && <FontAwesomeIcon icon={job.icon} style={{ fontSize: '15px' }} />}
+                        <div>{job.name}</div>
+
+                        <VerticalContainer>
+                        <Button onClick={() => incrementTotalVal(index)}> + </Button>
+                            <InputBox value={job.total} onChange={(data) => changeTotalVal(data, index)} />
+                            <Button onClick={() => decrementTotalVal(index)}> - </Button>
+                        </VerticalContainer>
+
+                        <span> / </span>
+
+                        <VerticalContainer>
+                        <Button onClick={() => incrementTargetlVal(index)}> + </Button>  
+                            <InputBox value={job.target} onChange={(data) => changeTargetlVal(data, index)} />
+                            <Button onClick={() => decrementTargetlVal(index)}> - </Button>
+                        </VerticalContainer>
+                        
+                    </Job>
+                ))}
+                3명 이하 인원만 가능해요!
             </>}
             
             {kind.name == "feadback" &&
