1ce802c48a9143cf915d4b55b0cb4cd41056822c
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-17 17:46:18 +0900

    Fix: iframe fix
    Fix: iframe fix
    
    Related to #6

--- fe/src/projectPage/ProjectPage.jsx
+++ fe/src/projectPage/ProjectPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35ec6f60340c715fa30f8ca391dd2f95afe24465fbaf0dbdd366ce2a12a552fe
-size 10315
+oid sha256:ff94325be887e754a513191eadb0029b3c6b4b1ee2203ea05339c24a178ce573
+size 10440
