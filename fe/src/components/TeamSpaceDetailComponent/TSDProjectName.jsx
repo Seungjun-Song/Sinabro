b63076f012dfe9be344f940017ff69cc8e7b78b7
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-17 15:46:55 +0900

    Fix: teamspace code fix
    Fix: teamspace code fix
    
    Related to #21

--- fe/src/components/TeamSpaceDetailComponent/TSDProjectName.jsx
+++ fe/src/components/TeamSpaceDetailComponent/TSDProjectName.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb62b31b92f31fcdefffffb70bf02252f35f75ec3d6886c39680e2f064a1e83d
-size 5880
+oid sha256:1704460cbbb28c336795ee30a2d399390263b46c70d2bd54f0074d6dd78c5c6e
+size 5976
