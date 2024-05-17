6509c81ded60f6130d58063ed196637a8baef00e
최재성 <we0620@naver.com>
2024-05-17 12:23:15 +0900

    Fix :젭라

--- fe/src/myPage/MyPageMainPanel.jsx
+++ fe/src/myPage/MyPageMainPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d02b5369720523046a000a30d6dda18608d59852198cbba0cfd909746d05ecab
-size 24132
+oid sha256:96bc0091289d17c9bce01b953e66c742772188f6a5c88dc58a375ba3dae1162e
+size 24253
