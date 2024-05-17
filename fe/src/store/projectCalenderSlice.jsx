57fc636385d2ad16f12de35b375c97b5f9acfb1b
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-17 17:05:31 +0900

    Fix: chat css fix
    Fix: chat css fix
    
    Related to #32

--- fe/src/store/projectCalenderSlice.jsx
+++ fe/src/store/projectCalenderSlice.jsx
@@ -1,3 +1,21 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f7be6452e99fa4783025524b11a3212214bc5e7739af0cc688bfaea3608923d8
-size 575
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    value: false
+}
+
+export const projectCalenderSlice =  createSlice({
+    name: 'projectCalender',
+    initialState,
+    reducers: {
+        changeProjectCalenderState: (state, action) => {
+            state.value = action.payload
+        },
+        toggleProjectCalenderState: (state, action) => {
+            state.value = !state.value
+        }
+    }
+})
+
+export const {changeProjectCalenderState, toggleProjectCalenderState} = projectCalenderSlice.actions
+export default projectCalenderSlice.reducer
--- fe/src/surveyPage/SkillBox.jsx
+++ fe/src/surveyPage/SkillBox.jsx
@@ -1,3 +1,55 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:373d657de2d4bc0d7751580137ece218d8ede5685704f9aaea0ae9cc63a45fab
-size 1355
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDesktop, faCog, faLeaf } from '@fortawesome/free-solid-svg-icons';
+
+const Box = styled.div`
+  padding: 10px;
+  gap: 10px;
+  border-radius: 5px;
+  max-height: 50px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 9rem;
+  box-shadow: 4px 4px 15px rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+
+  ${({ checked }) => !checked && `
+    transition: transform 0.3s ease;
+    &:hover {
+      transform: scale(1.05);
+    }
+  `}
+  
+  ${({ checked }) => checked && `
+    transform: scale(1.1);
+  `}
+`;
+
+const SkillBox = ({ iconName, borderColor, text, checked }) => {
+
+  let icon;
+  switch (iconName) {
+    case 'computer':
+      icon = faDesktop;
+      break;
+    case 'gear':
+      icon = faCog;
+      break;
+    case 'leaf':
+      icon = faLeaf;
+      break;
+    default:
+      icon = null;
+  }
+
+  return (
+    <Box style={{backgroundColor: checked ? borderColor  : "white" , border: `3px solid ${borderColor }`, color: `${checked ? "white" : borderColor}` }} checked={checked}>
+      {icon && <FontAwesomeIcon icon={icon} style={{ fontSize: '24px' }} />}
+      <span style={{ color: checked?  "white": 'black', fontWeight: 'bold' }}>{text}</span>
+    </Box>
+  );
+}
+
+export default SkillBox;
