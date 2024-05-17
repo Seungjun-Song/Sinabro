7d83e57ab296aea9cc54861a29f348d483870f2f
최재성 <we0620@naver.com>
2024-05-17 11:46:44 +0900

    Fix :수정중

--- fe/src/myPage/MemoryGraph.jsx
+++ fe/src/myPage/MemoryGraph.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b22f56c3190d1d15e427c00117175313036a47d5d6e590eecc2a8cf28648dc5f
-size 16657
+oid sha256:79a61b52f2a89e5f06660bc40e668bd58578fd295948ea6b23d2d5d3744aace1
+size 20715
--- fe/src/myPage/MyPageMainPanel.jsx
+++ fe/src/myPage/MyPageMainPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6d3c7b2302f46683af4d2fbe4d71d377eb68f04edfe22d47de5f599e50a0890
-size 23646
+oid sha256:5d467c0eed9d218474f155461fef3f48fde07aeecae925f4ef03f08c17e3d7df
+size 24139
