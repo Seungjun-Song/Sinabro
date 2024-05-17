4d67540f4e2f104653d57fd3101002e1351463bf
박주헌 <741u741@naver.com>
2024-05-17 21:09:48 +0900

    Fix: project page iframe

--- fe/src/projectPage/ProjectPage.jsx
+++ fe/src/projectPage/ProjectPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc38c6612782387c35bf7ee8baa1f673fb86e0c6777ec6d29f33f2dab1894b16
-size 10471
+oid sha256:883f484fa77d93e44ebd7fad488fc3401da3f3d3752026078c384d44425d0259
+size 10534
--- fe/src/projectPage/ProjectPageLeftPanel.jsx
+++ fe/src/projectPage/ProjectPageLeftPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e289fb15725238dc5aca504eba0e803cdf0cb86173a15d68552b33977d080170
-size 28533
+oid sha256:709ca29c487eee0dea8a7a1ef72035016b27c25f03533b867bd95b12c57cc9a9
+size 28665
