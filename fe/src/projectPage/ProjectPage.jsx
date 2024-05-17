4a75a61087d6627001d73b20bbdf676f2139ece7
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-17 09:38:30 +0900

    Fix: fix schedule update
    Fix: fix schedule update
    
    Related to #6

--- fe/src/projectPage/ProjectPage.jsx
+++ fe/src/projectPage/ProjectPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8aae3498fa2c9c12e563fd07ca256150dd2dc64f234e9ba8d186e21082bc140
-size 9669
+oid sha256:9a887b49f1fc82753f02395bce69ef7a59cc7941f62c0b5b307085f0b562cc85
+size 9799
