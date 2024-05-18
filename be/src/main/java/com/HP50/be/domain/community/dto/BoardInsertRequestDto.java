448ba6d61a6f4f0509922e3a869194e7128ff612
박종국 <whdrnrdl789@naver.com>
2024-05-18 15:42:11 +0900

    Fix: Check Project Exception

--- be/src/main/java/com/HP50/be/domain/community/dto/BoardInsertRequestDto.java
+++ be/src/main/java/com/HP50/be/domain/community/dto/BoardInsertRequestDto.java
@@ -1,3 +1,52 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9f52043d71d5241f484444d390a8df1f3e86e05d902301925eacd8ed3ee66c68
-size 1701
+package com.HP50.be.domain.community.dto;
+
+import com.HP50.be.domain.code.dto.TagDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Optional;
+
+@Data
+public class BoardInsertRequestDto {
+
+    @Schema(example = "1", description = "boardId를 전송 받으면 update\n" +
+                                            "전송받지 않는다면 save")
+    private Integer boardId;
+
+    @Schema(example = "리액트 사용 가능한 프론트엔드 개발자 구합니다.")
+    private String boardTitle;
+
+    @Schema(example = "저희는 백엔드 3명에 프론트 2명입니다 한분만 와주세요")
+    private String boardContent;
+
+    @Schema(example = "https://firebase.com/v4/jbbbejqhuabsaskdb.jpg")
+    private String boardImg;
+
+    @Schema(description = "피드백 주세요 일 경우에만 게시", example = "https://k10e103.p.ssafy.io/my-code-server")
+    private String projectLink;
+
+    @Schema(description = "팀 구해요 일 경우는 null", example = "11")
+    private Optional<Integer> projectId;
+
+    @Schema(example = "401", description = "401 = 팀원 구해요" +
+            "402 = 팀 원해요" +
+            "403 = 피드백 원해요")
+    private Integer subCategoryId;
+
+    @Schema(example = "2")
+    private Optional<Integer> requiredPeopleBackEnd = Optional.of(0);
+
+    @Schema(example = "1")
+    private Optional<Integer> requiredPeopleFrontEnd = Optional.of(0);
+
+    @Schema(example = "0")
+    private Optional<Integer> recruitedPeopleBackEnd = Optional.of(0);
+
+    @Schema(example = "0")
+    private Optional<Integer> recruitedPeopleFrontEnd = Optional.of(0);
+
+
+    private List<TagDto> boardTag;
+
+}
