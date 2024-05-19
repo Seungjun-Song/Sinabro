d7b43b4b5068a1a7d6e09c863aeca12bc6d0d210
전은희 <jeon78eun@naver.com>
2024-05-19 16:23:21 +0900

    Fix: 프로젝트 페이지 글자 수정
    프로젝트 페이지 글자 수정

--- fe/src/projectPage/ProjectPage.jsx
+++ fe/src/projectPage/ProjectPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e18f19dd6882a3eea491f905f5abf2b4dcc68bd0c446eb38cde9c04434f58ad
-size 14775
+oid sha256:79cfc1d30b206cae7d1fd844fbce67ce7ca472932acdb25b04988047382bfee6
+size 14782
