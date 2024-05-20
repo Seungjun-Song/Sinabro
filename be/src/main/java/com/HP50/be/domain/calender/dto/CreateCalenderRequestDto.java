3b6d281a64dbabc1aa22279fdd19a7a04ec4ce38
전은희 <jeon78eun@naver.com>
2024-05-20 11:30:14 +0900

    Upload New File

--- be/src/main/java/com/HP50/be/domain/calender/dto/CreateCalenderRequestDto.java
+++ be/src/main/java/com/HP50/be/domain/calender/dto/CreateCalenderRequestDto.java
@@ -1,3 +1,26 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:646cf0dba4539fa83ec6a53a5a891676be27c5644c0227739e01bdd7e801f2ec
-size 778
+package com.HP50.be.domain.calender.dto;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@Builder
+public class CreateCalenderRequestDto {
+    @Schema(example = "5", description = "프로젝트의 번호")
+    Integer projectId;
+    @Schema(example = "94429120", description = "담당자의 번호")
+    Integer managerId;
+    @Schema(example = "1", description = "선택사항임")
+    Integer milestoneId;
+    @Schema(example = "2024-05-12", description = "일정 시작일")
+    LocalDate calenderStartDt;
+    @Schema(example = "2024-05-18", description = "일정 마지막 일")
+    LocalDate calenderEndDt;
+    @Schema(example = "test 일정", description = "일정의 이름")
+    String calenderName;
+}
---
+++ exec/sinabro_1_빌드배포정리.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2190f018e711c6e05ba5c723dd11026a9e47e19a22ca87b6675f8269e86be1bb
+size 25496
