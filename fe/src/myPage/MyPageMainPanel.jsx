9edd890aad030f3b1c2623b50076ac319f436971
최재성 <we0620@naver.com>
2024-05-21 09:53:31 +0900

    Fix :수정 롤백

--- fe/src/myPage/MyPageMainPanel.jsx
+++ fe/src/myPage/MyPageMainPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db479ac6fd766e1dc1432a914deb510e6a56d809c34bca02234266a36f48c2ea
-size 27689
+oid sha256:c345e5ba17883f7bb1ecc910fcaca970ffc11585629308fdcb0923010f606247
+size 26236
