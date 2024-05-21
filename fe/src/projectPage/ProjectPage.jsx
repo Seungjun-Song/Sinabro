c722cc3ac3686543555440696bdd146f407a3f5f
박주헌 <139435006+JH201421228@users.noreply.github.com>
2024-05-21 09:51:54 +0900

    Fix: project darkmode

--- fe/src/projectPage/ProjectPage.jsx
+++ fe/src/projectPage/ProjectPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22e4a339a99474f887bc1666906dff010526948b83ee08072a0de6a4676c231d
-size 18360
+oid sha256:cd65451c4c307e0c9f4d37a6c9e95c8c13da4f74710d7263eed62fcdc8685cf3
+size 18391
