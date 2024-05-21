95a51ef2ad55731d16856a07465759ea2e07a733
김윤민 <qkslzl101216@naver.com>
2024-05-21 16:35:02 +0900

    Fix: SonarQube Alert Cancel

--- fe/src/store/inviteUserSlice.jsx
+++ fe/src/store/inviteUserSlice.jsx
@@ -1,3 +1,22 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:55a556af358fed13c890452067f016963c5443ec83a1ce0b048932054c3c165c
-size 492
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    value: null
+}
+
+export const inviteUserSlice =  createSlice({
+    name: 'inviteUser',
+    initialState,
+    reducers: {
+        setInviteUser: (state, action) => {
+            state.value = action.payload
+        },
+        clearInviteUser: (state) => {
+            state.value = null
+        },
+    }
+})
+
+export const {setInviteUser, clearInviteUser} = inviteUserSlice.actions
+
+export default inviteUserSlice.reducer
--- fe/src/store/userSearchSlice.jsx
+++ fe/src/store/userSearchSlice.jsx
@@ -1,3 +1,22 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d9f9c2078315ce7dc2dbff557f851820bee30adc635cf6965de4345008964ffc
-size 488
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    value: []
+}
+
+export const userSearchSlice =  createSlice({
+    name: 'userSearch',
+    initialState,
+    reducers: {
+        setUserSearch: (state, action) => {
+            state.value = action.payload
+        },
+        clearUserSearch: (state) => {
+            state.value = []
+        },
+    }
+})
+
+export const {setUserSearch, clearUserSearch} = userSearchSlice.actions
+
+export default userSearchSlice.reducer
