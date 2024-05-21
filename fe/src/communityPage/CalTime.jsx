4b2d05dd3bf06c62b9e659b2e1f6a3e1ee009ce0
김윤민 <qkslzl101216@naver.com>
2024-05-21 16:57:28 +0900

    Feat: Board Time Test

--- fe/src/communityPage/CalTime.jsx
+++ fe/src/communityPage/CalTime.jsx
@@ -1,32 +1,33 @@
-
 const CalTime = (fullDate) => {
-    //시간 계산해서 넣기
-    //오늘 올라온거라면 n시간 전, n분 전 표시
-    const nowDate = new Date();
+  //시간 계산해서 넣기
+  //오늘 올라온거라면 n시간 전, n분 전 표시
+  const nowDate = new Date();
 
-    let finalDate;
-    const day = fullDate.getDate();
+  let finalDate;
+  const day = fullDate.getDate();
 
-        if(day === nowDate.getDate()){
-            const hour = fullDate.getHours();
-            const minuet = fullDate.getMinutes();
-                    
-            if(hour === nowDate.getHours()){
-                const diff = nowDate.getMinutes() - minuet;
-                finalDate = diff + "분 전"
-            }
-            else{
-                const diff = nowDate.getHours() - hour;
-                finalDate = diff + "시간 전"
-            }
-        }
-        else{
-            finalDate = fullDate.getFullYear() + "." 
-                        + fullDate.getMonth() + "." 
-                        + fullDate.getDate();
-        }
+  if (day === nowDate.getDate()) {
+    const hour = fullDate.getHours();
+    const minuet = fullDate.getMinutes();
 
-        return finalDate;
-}
+    if (hour === nowDate.getHours()) {
+      const diff = nowDate.getMinutes() - minuet;
+      finalDate = diff + "분 전";
+    } else {
+      const diff = nowDate.getHours() - hour;
+      finalDate = diff + "시간 전";
+    }
+  } else {
+    console.log(`아니 왜 이럼? 살려줘요 ${finalDate}`);
+    finalDate =
+      fullDate.getFullYear() +
+      "." +
+      fullDate.getMonth() +
+      "." +
+      fullDate.getDate();
+  }
+
+  return finalDate;
+};
 
 export default CalTime;
